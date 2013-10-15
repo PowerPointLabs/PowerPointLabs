c95eda011813948f76b4f2867fe0aafd4e0404e7
raunak91 <none@none>
2013-10-16 06:55:01 +0800

    Fixed minimum rotation issue + add motion path object

---
+++ PowerPointLabs2007/PowerPointLabs2007/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PowerPointLabs2007
+{
+    public partial class Form1 : Form
+    {
+        public string newName;
+        private Ribbon1 ribbon;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(Ribbon1 parentRibbon, String oldName)
+            : this()
+        {
+            ribbon = parentRibbon;
+            newName = oldName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.newName = this.textBox1.Text;
+            ribbon.nameEdited(this.newName);
+            this.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Text = newName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
