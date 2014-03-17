8dc8f55d85d50cad7697d0b9aedca244e802b589
raunak91 <none@none>
2014-03-17 22:17:56 +0800

    Deployed V1.6.1

--- PowerPointLabs/PowerPointLabs/Ribbon1.cs
+++ PowerPointLabs/PowerPointLabs/Ribbon1.cs
@@ -1845,7 +1845,7 @@ namespace PowerPointLabs
         {
             //AboutForm form = new AboutForm();
             //form.Show();
-            System.Windows.Forms.MessageBox.Show("          PowerPointLabs Plugin Version 1.6 [Release date: 15 Mar 2014]\n     Developed at School of Computing, National University of Singapore.\n        For more information, visit our website http://PowerPointLabs.info", "About PowerPointLabs");
+            System.Windows.Forms.MessageBox.Show("          PowerPointLabs Plugin Version 1.6.1 [Release date: 17 Mar 2014]\n     Developed at School of Computing, National University of Singapore.\n        For more information, visit our website http://PowerPointLabs.info", "About PowerPointLabs");
         }
         public void HelpButtonClick(Office.IRibbonControl control)
         {
