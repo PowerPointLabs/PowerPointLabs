70601fd4e9263ccfa18eb3251655d07e6e15c4b8
raunak91 <none@none>
2014-03-31 05:30:54 +0800

    Added highlight bullets to context menu of shapes + deployed V1.7.1

--- PowerPointLabs/PowerPointLabs/Ribbon1.cs
+++ PowerPointLabs/PowerPointLabs/Ribbon1.cs
@@ -3006,7 +3006,7 @@ namespace PowerPointLabs
         {
             //AboutForm form = new AboutForm();
             //form.Show();
-            System.Windows.Forms.MessageBox.Show("          PowerPointLabs Plugin Version 1.7 [Release date: 27 Mar 2014]\n     Developed at School of Computing, National University of Singapore.\n        For more information, visit our website http://PowerPointLabs.info", "About PowerPointLabs");
+            System.Windows.Forms.MessageBox.Show("          PowerPointLabs Plugin Version 1.7.1 [Release date: 31 Mar 2014]\n     Developed at School of Computing, National University of Singapore.\n        For more information, visit our website http://PowerPointLabs.info", "About PowerPointLabs");
         }
         public void HelpButtonClick(Office.IRibbonControl control)
         {
