e82a7c3b028a62f23a60f4b4d2209f15f37a86d2
Karan Kamath <karankamath26@gmail.com>
2014-06-12 17:25:48 +0800

    Add color to picker on LMouseButton Release

--- PowerPointLabs/PowerPointLabs/Views/ColorPickerForm.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Views/ColorPickerForm.Designer.cs
@@ -15,21 +15,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(191, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(81, 45);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current Color";
             // 
             // ColorPickerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Name = "ColorPickerForm";
             this.Text = "ColorPickerForm";
             this.Load += new System.EventHandler(this.ColorPickerForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
 
     }
 }
--- PowerPointLabs/PowerPointLabs/Views/ColorPickerForm.cs
+++ PowerPointLabs/PowerPointLabs/Views/ColorPickerForm.cs
@@ -13,30 +13,41 @@ namespace PowerPointLabs.Views
 {
     public partial class ColorPickerForm : Form
     {
-        [DllImport("Gdi32.dll")]
+        // Refactor this to Native
+
+        [DllImport("Gdi32.dll", CallingConvention = CallingConvention.StdCall)]
+
         public static extern int GetPixel(
         System.IntPtr hdc,    // handle to DC
         int nXPos,  // x-coordinate of pixel
         int nYPos   // y-coordinate of pixel
         );
 
-        [DllImport("User32.dll")]
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall)]
         public static extern IntPtr GetDC(IntPtr wnd);
 
-        [DllImport("User32.dll")]
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall)]
         public static extern void ReleaseDC(IntPtr dc);
-
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Timers.Timer timer1;
 
         public ColorPickerForm()
         {
-            SetUp();
             InitializeComponent();
             this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.timer1.Start();
+            //start
+            _native = new LMouseListener();
+            _native.RButtonClicked +=
+                 new EventHandler<SysMouseEventInfo>(_native_RButtonClicked);
         }
 
+        LMouseListener _native;
+
+        void _native_RButtonClicked(object sender, SysMouseEventInfo e)
+        {
+            this.Activate();
+            timer1.Stop();
+            _native.Close();
+        }
         public ColorPickerForm(PowerPoint.ShapeRange selectedShapes)
             : this()
         {
@@ -48,39 +59,6 @@ namespace PowerPointLabs.Views
 
         }
 
-        private void SetUp()
-        {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.timer1 = new System.Timers.Timer();
-            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
-            this.SuspendLayout();
-            //
-            // panel1
-            //
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Location = new System.Drawing.Point(216, 8);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(64, 56);
-            this.panel1.TabIndex = 0;
-            //
-            // timer1
-            //
-            this.timer1.Enabled = true;
-            this.timer1.SynchronizingObject = this;
-            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
-            //
-            // ColorPickerForm
-            //
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.panel1);
-            this.Name = "ColorPickerForm";
-            this.Text = "ColorPickerForm";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ColorPickerForm_Paint);
-            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
-            this.ResumeLayout(false);
-        }
 
         private void ColorPickerForm_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
         {
@@ -98,7 +76,7 @@ namespace PowerPointLabs.Views
             Point p = Control.MousePosition;
             IntPtr dc = GetDC(IntPtr.Zero);
             this.panel1.BackColor = ColorTranslator.FromWin32(GetPixel(dc, p.X, p.Y));
-            ReleaseDC(dc);
+
         }
 
         /// <summary>
@@ -114,6 +92,142 @@ namespace PowerPointLabs.Views
                 }
             }
             base.Dispose(disposing);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point p = Control.MousePosition;
+            IntPtr dc = GetDC(IntPtr.Zero);
+            this.panel1.BackColor = ColorTranslator.FromWin32(GetPixel(dc, p.X, p.Y));
+
+            this.Activate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    public class SysMouseEventInfo : EventArgs
+    {
+        public string WindowTitle { get; set; }
+    }
+    public class LMouseListener
+    {
+        public LMouseListener()
+        {
+            this.CallBack += new HookProc(MouseEvents);
+            //Module mod = Assembly.GetExecutingAssembly().GetModules()[0];
+            //IntPtr hMod = Marshal.GetHINSTANCE(mod);
+            using (System.Diagnostics.Process process = System.Diagnostics.Process.GetCurrentProcess())
+            using (System.Diagnostics.ProcessModule module = process.MainModule)
+            {
+                IntPtr hModule = GetModuleHandle(module.ModuleName);
+                _hook = SetWindowsHookEx(WH_MOUSE_LL, this.CallBack, hModule, 0);
+                //if (_hook != IntPtr.Zero)
+                //{
+                //    Console.WriteLine("Started");
+                //}
+            }
+        }
+        int WH_MOUSE_LL = 14;
+        int HC_ACTION = 0;
+        HookProc CallBack = null;
+        IntPtr _hook = IntPtr.Zero;
+
+        public event EventHandler<SysMouseEventInfo> RButtonClicked;
+
+        int MouseEvents(int code, IntPtr wParam, IntPtr lParam)
+        {
+            //Console.WriteLine("Called");
+
+            if (code < 0)
+                return CallNextHookEx(_hook, code, wParam, lParam);
+
+            if (code == this.HC_ACTION)
+            {
+                // Left button pressed somewhere
+                if (wParam.ToInt32() == (uint)WM.WM_LBUTTONUP)
+                {
+                    MSLLHOOKSTRUCT ms = new MSLLHOOKSTRUCT();
+                    ms = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                    IntPtr win = WindowFromPoint(ms.pt);
+                    string title = GetWindowTextRaw(win);
+                    if (RButtonClicked != null)
+                    {
+                        RButtonClicked(this, new SysMouseEventInfo { WindowTitle = title });
+                    }
+                }
+            }
+            return CallNextHookEx(_hook, code, wParam, lParam);
+        }
+
+        public void Close()
+        {
+            if (_hook != IntPtr.Zero)
+            {
+                UnhookWindowsHookEx(_hook);
+            }
+        }
+        public delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "SetWindowsHookEx", SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(int xPoint, int yPoint);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(POINT Point);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, [Out] StringBuilder lParam);
+
+        public static string GetWindowTextRaw(IntPtr hwnd)
+        {
+            // Allocate correct string length first
+            //int length = (int)SendMessage(hwnd, (int)WM.WM_GETTEXTLENGTH, IntPtr.Zero, IntPtr.Zero);
+            StringBuilder sb = new StringBuilder(65535);//THIS COULD BE BAD. Maybe you shoudl get the length
+            SendMessage(hwnd, (int)WM.WM_GETTEXT, (IntPtr)sb.Capacity, sb);
+            return sb.ToString();
+        }
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public int mouseData;
+        public int flags;
+        public int time;
+        public UIntPtr dwExtraInfo;
+    }
+    enum WM : uint
+    {//all windows messages here
+        WM_LBUTTONUP = 0x0202,
+        WM_GETTEXT = 0x000D,
+        WM_GETTEXTLENGTH = 0x000E
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
         }
     }
 }
