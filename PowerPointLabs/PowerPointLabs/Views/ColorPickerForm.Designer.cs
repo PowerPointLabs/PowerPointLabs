b87e05b1b42a3769762a8b6f4bf8458a7b455359
Karan Kamath <karankamath26@gmail.com>
2014-06-13 16:29:16 +0800

    Added some logic for drag drop

--- PowerPointLabs/PowerPointLabs/Views/ColorPickerForm.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Views/ColorPickerForm.Designer.cs
@@ -30,6 +30,7 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(81, 45);
             this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // timer1
