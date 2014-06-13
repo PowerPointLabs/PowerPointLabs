4c6eb57f79f4a7513465e04700d75d176548ba19
Karan Kamath <karankamath26@gmail.com>
2014-06-13 16:41:46 +0800

    Add error dialog wrapper

---
+++ PowerPointLabs/PowerPointLabs/ErrorDialogWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PowerPointLabs
+{
+    class ErrorDialogWrapper
+    {
+        public static void showDialog(string title, string message, string details)
+        {
+            var dialogTypeName = "System.Windows.Forms.PropertyGridInternal.GridErrorDlg";
+            var dialogType = typeof(Form).Assembly.GetType(dialogTypeName);
+
+            // Create dialog instance.
+            var dialog = (Form)Activator.CreateInstance(dialogType, new PropertyGrid());
+
+            // Populate relevant properties on the dialog instance.
+            dialog.Text = title;
+            dialogType.GetProperty("Details").SetValue(dialog, details, null);
+            dialogType.GetProperty("Message").SetValue(dialog, message, null);
+
+            var result = dialog.ShowDialog();
+        }
+    }
+}
