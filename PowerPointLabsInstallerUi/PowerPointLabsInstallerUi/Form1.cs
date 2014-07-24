599196e7c73d2d9226911bb6e00f7a83f4ecaa4f
Kai <gigikie@gmail.com>
2014-07-24 16:10:25 +0800

    impl PowerPointLabs offline installer ui program

---
+++ PowerPointLabsInstallerUi/PowerPointLabsInstallerUi/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace PowerPointLabsInstallerUi
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _installerZipAddress = Path.Combine(Directory.GetCurrentDirectory(), "data.zip");
+        private const string TextButtonClose = "Close";
+        private const string TextButtonRunning = "Running...";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text != TextButtonClose)
+            {
+                button1.Enabled = false;
+                button1.Text = TextButtonRunning;
+
+                
+                Boolean isUnzipSuccessful = UnzipInstaller(_installerZipAddress);
+                if (isUnzipSuccessful)
+                {
+                    RunInstaller();
+                }
+
+                button1.Enabled = true;
+                button1.Text = TextButtonClose;
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private static void RunInstaller()
+        {
+            try
+            {
+                var process = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = Path.Combine(Path.GetTempPath(), @"PowerPointLabsInstaller\setup.exe"),
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    }
+                };
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                PowerPointLabs.Views.ErrorDialogWrapper.ShowDialog("Failed to install",
+                    "An error occurred while installing PowerPointLabs", e);
+            }
+        }
+
+        private Boolean UnzipInstaller(String installerZipAddress)
+        {
+            try
+            {
+                var installerZip = ZipStorer.Open(installerZipAddress, FileAccess.Read);
+                var zipDir = installerZip.ReadCentralDir();
+                foreach (var file in zipDir)
+                {
+                    installerZip.ExtractFile(file,
+                        Path.Combine(Path.GetTempPath(), @"PowerPointLabsInstaller\" + file.FilenameInZip));
+                }
+                installerZip.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                PowerPointLabs.Views.ErrorDialogWrapper.ShowDialog("Failed to install",
+                    "An error occurred while installing PowerPointLabs", e);
+            }
+            return false;
+        }
+    }
+}
---
+++ PowerPointLabsInstallerUi/PowerPointLabsInstallerUi/TextCollection.cs
@@ -0,0 +1,10 @@
+﻿namespace PowerPointLabs
+{
+    internal class TextCollection
+    {
+        # region Error Dialog
+        public const string UserFeedBack = " Help us fix the problem by emailing ";
+        public const string Email = @"pptlabs@comp.nus.edu.sg";
+        # endregion
+    }
+}
