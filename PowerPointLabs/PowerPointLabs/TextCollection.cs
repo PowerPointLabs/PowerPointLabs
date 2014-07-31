f347536d2198b58614e7e0e71796f8f2e1abc87f
Jerome Cheng <jerome@ayulin.net>
2014-07-31 10:30:31 +0800

    Rephrase recorder messages.

--- PowerPointLabs/PowerPointLabs/TextCollection.cs
+++ PowerPointLabs/PowerPointLabs/TextCollection.cs
@@ -205,19 +205,19 @@
         public const string RecorderRecordingStatusLabel = "Recording...";
         public const string RecorderPlayingStatusLabel = "Playing...";
         public const string RecorderPauseStatusLabel = "Pause";
-        public const string RecorderUnrecognizeAudio = "Unrecognize Embedded Audio";
+        public const string RecorderUnrecognizeAudio = "Unrecognized Embedded Audio";
         public const string RecorderScriptStatusNoAudio = "No Audio";
         public const string RecorderWndMessageError = "Fatal error";
         public const string RecorderNoScriptDetail = "No Script Available";
-        public const string RecorderNoInputDeviceMsg = "No Input Device suitable for the recording.\n" +
-                                                       "Make sure your computer has a built-in voice picker and has been enabled, " +
-                                                       "or an external voice input device has been connected.";
+        public const string RecorderNoInputDeviceMsg = "No audio input device was found.\n" +
+                                                       "Check that a microphone or other audio input device is attached " +
+                                                       "and working.";
         public const string RecorderNoInputDeviceMsgBoxTitle = "Input Device Not Found";
-        public const string RecorderSaveRecordMsg = "Do you want to save the record?";
-        public const string RecorderSaveRecordMsgBoxTitle = "Save Record";
-        public const string RecorderReplaceRecordMsgFormat = "Do you want to replace\n{0}\nwith current record?";
-        public const string RecorderReplaceRecordMsgBoxTitle = "Replacement";
-        public const string RecorderNoRecordToPlayError = "No record to play back. Please record first.";
+        public const string RecorderSaveRecordMsg = "Do you want to save the recording?";
+        public const string RecorderSaveRecordMsgBoxTitle = "Save Recording";
+        public const string RecorderReplaceRecordMsgFormat = "Do you want to replace\n{0}\nwith the current recording?";
+        public const string RecorderReplaceRecordMsgBoxTitle = "Replace Audio";
+        public const string RecorderNoRecordToPlayError = "There are no recordings to play.";
         public const string RecorderInvalidOperation = "Invalid Operation";
         # endregion
 
