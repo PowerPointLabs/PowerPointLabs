1dea308da3d55d738e1e4357ebe8088285ae6dd0
Huang Da <a0091847@nus.edu.sg>
2014-08-06 18:01:01 +0800

    update version info for dev 1.8.12.3

--- PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
@@ -43,7 +43,7 @@ namespace PowerPointLabs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.8.12.2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.8.12.3")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
