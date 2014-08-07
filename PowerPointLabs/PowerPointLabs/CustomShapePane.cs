e93d9051d85efb02a4f9c05a98d2701ffecc003d
Huang Da <a0091847@nus.edu.sg>
2014-08-07 10:42:00 +0800

    override CreateParams to speed up the shapes lab, but doesn't seem to be helpful

--- PowerPointLabs/PowerPointLabs/CustomShapePane.cs
+++ PowerPointLabs/PowerPointLabs/CustomShapePane.cs
@@ -699,6 +699,16 @@ namespace PowerPointLabs
 
             ClickTimerReset();
         }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
         # endregion
 
         # region Comparer
