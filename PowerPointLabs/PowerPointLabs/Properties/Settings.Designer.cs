673203b43052294c24010698866d5028c0429dbd
Huang Da <a0091847@nus.edu.sg>
2014-08-25 23:09:33 +0800

    update version info for release 1.9.0.4 (offline)

--- PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
@@ -34,7 +34,7 @@ namespace PowerPointLabs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("online")]
+        [global::System.Configuration.DefaultSettingValueAttribute("offline")]
         public string InstallerType {
             get {
                 return ((string)(this["InstallerType"]));
