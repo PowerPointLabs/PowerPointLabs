ea895beb7ff2a54bccb2d1eca492828330ce5b8b
Huang Da <a0091847@nus.edu.sg>
2015-03-04 04:15:41 +0800

    update for version dev 2.2.1.0 online

--- PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
@@ -43,7 +43,7 @@ namespace PowerPointLabs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.2.0.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.2.1.0")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
@@ -52,7 +52,7 @@ namespace PowerPointLabs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("14 Feb 2015")]
+        [global::System.Configuration.DefaultSettingValueAttribute("04 Mar 2015")]
         public string ReleaseDate {
             get {
                 return ((string)(this["ReleaseDate"]));
