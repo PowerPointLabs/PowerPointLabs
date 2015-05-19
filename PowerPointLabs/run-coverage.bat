c0a79d31d9473b0c9cab63733b8b1084b275f0b3
Kai <gigikie@gmail.com>
2015-05-20 04:32:26 +0800

    open coverage report aft go coverage

--- PowerPointLabs/run-coverage.bat
+++ PowerPointLabs/run-coverage.bat
@@ -1,3 +1,4 @@
 .\packages\OpenCover.4.5.3723\OpenCover.Console.exe -register:user -target:vstest.console.exe -targetargs:"/inIsolation .\UnitTestProject1\bin\Debug\UnitTestProject1.dll" -filter:+[*]* -output:.\cov-report.xml
 .\packages\ReportGenerator.2.1.4.0\ReportGenerator.exe -reports:.\cov-report.xml -targetdir:.\cov-report -reporttypes:Html
+start .\cov-report\index.htm
 pause
