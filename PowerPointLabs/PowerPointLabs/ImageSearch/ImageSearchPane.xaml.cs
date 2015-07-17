2d5c288cc00b3252efb2d39367ca23f4537c6367
Kai <gigikie@gmail.com>
2015-07-17 16:34:59 +0800

    simplify preview styles to direct text, blur, and blur textbox. other styles to be achieved thru options, otherwise too slow to generate

--- PowerPointLabs/PowerPointLabs/ImageSearch/ImageSearchPane.xaml.cs
+++ PowerPointLabs/PowerPointLabs/ImageSearch/ImageSearchPane.xaml.cs
@@ -33,7 +33,7 @@ namespace PowerPointLabs.ImageSearch
 
         public PowerPointPresentation PreviewPresentation { get; set; }
 
-        private Timer _previewTimer = new Timer { Interval = 500 };
+        private Timer _previewTimer = new Timer { Interval = 2000 };
 
         private readonly string _loadingImgPath = Path.GetTempPath() + "loading" + DateTime.Now.GetHashCode();
 
@@ -72,11 +72,11 @@ namespace PowerPointLabs.ImageSearch
             // TODO: Store this API somewhere...
             var api =
                 "https://www.googleapis.com/customsearch/v1?filter=1&cx=017201692871514580973%3Awwdg7q__" +
-//                "mb4&imgSize=large&searchType=image&imgType=photo&safe=medium&key=AIzaSyCGcq3O8NN9U7YX-Pj3E7tZde0yaFFeUyY";
+                "mb4&imgSize=large&searchType=image&imgType=photo&safe=medium&key=AIzaSyCGcq3O8NN9U7YX-Pj3E7tZde0yaFFeUyY";
 //                "mb4&imgSize=large&searchType=image&imgType=photo&safe=medium&key=AIzaSyDQeqy9efF_ASgi2dk3Ortj2QNnz90RdOw";
 //                "mb4&imgSize=large&searchType=image&imgType=photo&safe=medium&key=AIzaSyDXR8wBYL6al5jXIXTHpEF28CCuvL0fjKk";
 //                "mb4&imgSize=large&searchType=image&imgType=photo&safe=medium&key=AIzaSyAur2Fc0ewRyGK0U8NCaaEfuY0g_sx-Qwk";
-                "mb4&imgSize=large&searchType=image&imgType=photo&safe=medium&key=AIzaSyArj45s-GLXKX8NSM6HGdSFtRvAMuKE2p0";
+//                "mb4&imgSize=large&searchType=image&imgType=photo&safe=medium&key=AIzaSyArj45s-GLXKX8NSM6HGdSFtRvAMuKE2p0";
             var query = SearchTextBox.Text;
             // TODO: what if query is empty ... may need escape as well
 
@@ -177,6 +177,7 @@ namespace PowerPointLabs.ImageSearch
                     if (SearchListBox.SelectedValue != null
                         && (SearchListBox.SelectedValue as ImageItem).FullSizeImageFile == null)
                     {
+                        PreviewProgressRing.IsActive = true;
                         var fullsizeImageFile = Path.GetTempPath() + Guid.NewGuid();
                         var image = (ImageItem) SearchListBox.SelectedValue;
                         new Downloader()
@@ -209,16 +210,17 @@ namespace PowerPointLabs.ImageSearch
             // ui thread
             Dispatcher.BeginInvoke(new Action(() =>
             {
+                var selectedId = PreviewListBox.SelectedIndex;
                 PreviewProgressRing.IsActive = true;
                 PreviewList.Clear();
 
-                var previewFile = Path.GetTempPath() + "original" + DateTime.Now.GetHashCode();
+                var previewFile = Path.GetTempPath() + "original" + DateTime.Now.GetHashCode(); // --> opt
                 var previewFile2 = Path.GetTempPath() + "directText" + DateTime.Now.GetHashCode();
-                var previewFile3 = Path.GetTempPath() + "overlay" + DateTime.Now.GetHashCode();
-                var previewFile4 = Path.GetTempPath() + "textbox" + DateTime.Now.GetHashCode();
+                var previewFile3 = Path.GetTempPath() + "overlay" + DateTime.Now.GetHashCode(); // --> opt for direct text
+                var previewFile4 = Path.GetTempPath() + "textbox" + DateTime.Now.GetHashCode(); // --> opt for blur textbox
                 var previewFile5 = Path.GetTempPath() + "blur" + DateTime.Now.GetHashCode();
                 var previewFile6 = Path.GetTempPath() + "blur_textbox" + DateTime.Now.GetHashCode();
-                var previewFile7 = Path.GetTempPath() + "blur_part" + DateTime.Now.GetHashCode();
+                var previewFile7 = Path.GetTempPath() + "blur_part" + DateTime.Now.GetHashCode(); // --> opt for blur
 
                 // TODO multi thread
                 // TODO DRY
@@ -254,11 +256,11 @@ namespace PowerPointLabs.ImageSearch
                 FitToSlide.AutoFit(imageShape, PreviewPresentation);
                 imageShape.ZOrder(MsoZOrderCmd.msoSendToBack);
 
-                thisSlide.GetNativeSlide().Export(previewFile, "JPG");
-                PreviewList.Add(new ImageItem
-                {
-                    ImageFile = previewFile
-                });
+//                thisSlide.GetNativeSlide().Export(previewFile, "JPG");
+//                PreviewList.Add(new ImageItem
+//                {
+//                    ImageFile = previewFile
+//                });
                 // Original Preview done here
 
                 // Textbox style 1 starts
@@ -298,26 +300,26 @@ namespace PowerPointLabs.ImageSearch
                     ImageFile = previewFile2
                 });
                 // Textbox style 1 ends
-                // Textbox style 2 starts
-                var overlayShape = thisSlide.Shapes.AddShape(MsoAutoShapeType.msoShapeRectangle,
-                                                                0,
-                                                                0,
-                                                                PowerPointPresentation.Current.SlideWidth,
-                                                                PowerPointPresentation.Current.SlideHeight);
-                overlayShape.Fill.ForeColor.RGB = Utils.Graphics.ConvertColorToRgb(Color.Black);
-                overlayShape.Fill.Transparency = 0.65f;
-                overlayShape.Line.Visible = MsoTriState.msoFalse;
-                overlayShape.ZOrder(MsoZOrderCmd.msoSendToBack);
-                imageShape.ZOrder(MsoZOrderCmd.msoSendToBack);
-                thisSlide.GetNativeSlide().Export(previewFile3, "JPG");
-                PreviewList.Add(new ImageItem
-                {
-                    ImageFile = previewFile3
-                });
-                overlayShape.Delete();
+//                // Textbox style 2 starts
+//                var overlayShape = thisSlide.Shapes.AddShape(MsoAutoShapeType.msoShapeRectangle,
+//                                                                0,
+//                                                                0,
+//                                                                PowerPointPresentation.Current.SlideWidth,
+//                                                                PowerPointPresentation.Current.SlideHeight);
+//                overlayShape.Fill.ForeColor.RGB = Utils.Graphics.ConvertColorToRgb(Color.Black);
+//                overlayShape.Fill.Transparency = 0.65f;
+//                overlayShape.Line.Visible = MsoTriState.msoFalse;
+//                overlayShape.ZOrder(MsoZOrderCmd.msoSendToBack);
+//                imageShape.ZOrder(MsoZOrderCmd.msoSendToBack);
+//                thisSlide.GetNativeSlide().Export(previewFile3, "JPG");
+//                PreviewList.Add(new ImageItem
+//                {
+//                    ImageFile = previewFile3
+//                });
+//                overlayShape.Delete();
                 //
                 // textbox style 5 starts
-                overlayShape = thisSlide.Shapes.AddShape(MsoAutoShapeType.msoShapeRectangle,
+                var overlayShape = thisSlide.Shapes.AddShape(MsoAutoShapeType.msoShapeRectangle,
                                                                 0,
                                                                 0,
                                                                 PowerPointPresentation.Current.SlideWidth,
@@ -416,45 +418,46 @@ namespace PowerPointLabs.ImageSearch
 
                 //
                 // Textbox style 3 starts
-                foreach (PowerPoint.Shape shape in thisSlide.Shapes)
-                {
-                    if (shape.Type == MsoShapeType.msoPlaceholder
-                        || shape.Type == MsoShapeType.msoTextBox)
-                    {
-                        if (shape.TextFrame.HasText == MsoTriState.msoFalse
-                            || shape.Tags["GotHighlighted"].Trim().Length != 0)
-                        {
-                            continue;
-                        }
-                        // multiple paragraphs.. 
-                        foreach (TextRange2 paragraph in shape.TextFrame2.TextRange.Paragraphs)
-                        {
-                            if (paragraph.TrimText().Length > 0)
-                            {
-                                var highlightShape = thisSlide.Shapes.AddShape(MsoAutoShapeType.msoShapeRectangle,
-                                                                paragraph.BoundLeft - 5,
-                                                                paragraph.BoundTop - 5,
-                                                                paragraph.BoundWidth + 10,
-                                                                paragraph.BoundHeight + 10);
-                                highlightShape.Fill.ForeColor.RGB = Utils.Graphics.ConvertColorToRgb(Color.Black); // TODO customize
-                                highlightShape.Line.Visible = MsoTriState.msoFalse;
-                                Utils.Graphics.MoveZToJustBehind(highlightShape, shape);
-                                highlightShape.Name = "PPTLabsHighlightBackgroundShape" + DateTime.Now.ToString("yyyyMMddHHmmssffff");
-                                highlightShape.Tags.Add("HighlightBackground", shape.Name);
-                                shape.Tags.Add("GotHighlighted", highlightShape.Name);
-                            }
-                        }
-                    }
-                }
-                thisSlide.GetNativeSlide().Export(previewFile4, "JPG");
-                PreviewList.Add(new ImageItem
-                {
-                    ImageFile = previewFile4
-                });
+//                foreach (PowerPoint.Shape shape in thisSlide.Shapes)
+//                {
+//                    if (shape.Type == MsoShapeType.msoPlaceholder
+//                        || shape.Type == MsoShapeType.msoTextBox)
+//                    {
+//                        if (shape.TextFrame.HasText == MsoTriState.msoFalse
+//                            || shape.Tags["GotHighlighted"].Trim().Length != 0)
+//                        {
+//                            continue;
+//                        }
+//                        // multiple paragraphs.. 
+//                        foreach (TextRange2 paragraph in shape.TextFrame2.TextRange.Paragraphs)
+//                        {
+//                            if (paragraph.TrimText().Length > 0)
+//                            {
+//                                var highlightShape = thisSlide.Shapes.AddShape(MsoAutoShapeType.msoShapeRectangle,
+//                                                                paragraph.BoundLeft - 5,
+//                                                                paragraph.BoundTop - 5,
+//                                                                paragraph.BoundWidth + 10,
+//                                                                paragraph.BoundHeight + 10);
+//                                highlightShape.Fill.ForeColor.RGB = Utils.Graphics.ConvertColorToRgb(Color.Black); // TODO customize
+//                                highlightShape.Line.Visible = MsoTriState.msoFalse;
+//                                Utils.Graphics.MoveZToJustBehind(highlightShape, shape);
+//                                highlightShape.Name = "PPTLabsHighlightBackgroundShape" + DateTime.Now.ToString("yyyyMMddHHmmssffff");
+//                                highlightShape.Tags.Add("HighlightBackground", shape.Name);
+//                                shape.Tags.Add("GotHighlighted", highlightShape.Name);
+//                            }
+//                        }
+//                    }
+//                }
+//                thisSlide.GetNativeSlide().Export(previewFile4, "JPG");
+//                PreviewList.Add(new ImageItem
+//                {
+//                    ImageFile = previewFile4
+//                });
 
                 //
                 // dont affect next time preview
                 thisSlide.Delete();
+                PreviewListBox.SelectedIndex = selectedId;
 
                 // try catch finally?
                 PreviewProgressRing.IsActive = false;
