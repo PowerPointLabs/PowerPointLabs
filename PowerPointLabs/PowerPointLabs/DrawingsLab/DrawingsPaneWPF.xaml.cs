6c5c2f8d3cbfc4e28dea1edca0cb3f1a6532f167
Ohohcakester <ohohgit@gmail.com>
2015-07-21 16:01:17 +0800

    Transfer UI logic from DrawingsPane.cs to DrawingsPaneWPF.xaml.cs

--- PowerPointLabs/PowerPointLabs/DrawingsLab/DrawingsPaneWPF.xaml.cs
+++ PowerPointLabs/PowerPointLabs/DrawingsLab/DrawingsPaneWPF.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -13,6 +14,7 @@ using System.Windows.Navigation;
 using Microsoft.Office.Core;
 using Microsoft.Office.Interop.PowerPoint;
 using PowerPointLabs.Models;
+using PPExtraEventHelper;
 using Shape = System.Windows.Shapes.Shape;
 
 namespace PowerPointLabs.DrawingsLab
@@ -22,10 +24,61 @@ namespace PowerPointLabs.DrawingsLab
     /// </summary>
     public partial class DrawingsPaneWPF
     {
+        private static bool hotkeysInitialised = false;
+
         public DrawingsPaneWPF()
         {
             InitializeComponent();
+
+            InitialiseHotkeys();
+
+            BindDataToPanels();
+
+            InitToolTipControl();
         }
+
+        #region ToolTip
+        private void InitToolTipControl()
+        {
+            //toolTip1.SetToolTip(panel1, TextCollection.ColorsLabText.MainColorBoxTooltips);
+        }
+        #endregion
+
+        #region DataBindings
+        private void BindDataToPanels()
+        {
+            //this.panel1.DataBindings.Add(new CustomBinding(
+                //"BackColor",
+                //dataSource,
+                //"selectedColor",
+                //new Converters.HSLColorToRGBColor()));
+        }
+        #endregion
+
+
+        #region HotkeyInitialisation
+        private bool IsPanelOpen()
+        {
+            var drawingsPane = Globals.ThisAddIn.GetActivePane(typeof(DrawingsPane));
+            return drawingsPane.Visible;
+        }
+
+        private Action RunOnlyWhenOpen(Action action)
+        {
+            return () => { if (IsPanelOpen()) action(); };
+        }
+
+        private void InitialiseHotkeys()
+        {
+            if (hotkeysInitialised) return;
+            hotkeysInitialised = true;
+
+            PPKeyboard.AddKeyupAction(Native.VirtualKey.VK_L, RunOnlyWhenOpen(DrawingsLabMain.SwitchToLineTool));
+            PPKeyboard.AddKeyupAction(Native.VirtualKey.VK_H, RunOnlyWhenOpen(DrawingsLabMain.HideTool));
+            PPKeyboard.AddKeyupAction(Native.VirtualKey.VK_D, RunOnlyWhenOpen(DrawingsLabMain.CloneTool));
+            PPKeyboard.AddKeyupAction(Native.VirtualKey.VK_F, RunOnlyWhenOpen(DrawingsLabMain.MultiCloneTool));
+        }
+        #endregion
 
         private void LineButton_Click(object sender, EventArgs e)
         {
