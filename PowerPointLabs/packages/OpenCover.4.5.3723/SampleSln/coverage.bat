a4ed9684560557da2614513a11ec3f75d9d5fc6d
Kai <gigikie@gmail.com>
2015-09-28 01:24:17 +0800

    remove UT and unused packages

--- PowerPointLabs/packages/OpenCover.4.5.3723/SampleSln/coverage.bat
+++
@@ -1,5 +0,0 @@
-.\packages\OpenCover.4.5.3723\OpenCover.Console.exe -register:user "-filter:+[Bom]* -[*Test]*" "-target:.\packages\NUnit.Runners.2.6.3\tools\nunit-console-x86.exe" "-targetargs:/noshadow .\BomTest\bin\Debug\BomTest.dll"
-
-.\packages\ReportGenerator.1.9.1.0\ReportGenerator.exe "-reports:results.xml" "-targetdir:.\coverage"
-
-pause
