ff9ada63901b62760cf51f38d0639453f6d1ba5f
Kai <gigikie@gmail.com>
2015-09-28 00:50:53 +0800

    clean temp images when start images lab

--- PowerPointLabs/PowerPointLabs/ImageSearch/Util/TempPath.cs
+++ PowerPointLabs/PowerPointLabs/ImageSearch/Util/TempPath.cs
@@ -8,10 +8,16 @@ namespace PowerPointLabs.ImageSearch.Util
     class TempPath
     {
         // resources & const
-        public static string TempFolder = Path.GetTempPath() + "pptlabs_imagesLab"
+        public static string AggregatedTempFolder = Path.GetTempPath() + "pptlabs_imagesLab" + @"\";
+
+        public static string AggregatedBackupTempFolder =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\"
+            + "pptlabs_imagesLab" + @"\";
+
+        public static string TempFolder = AggregatedTempFolder + "pptlabs_imagesLab"
                                                    + DateTime.Now.GetHashCode() + @"\";
-        public static readonly string BackupTempFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\"
-                + "pptlabs_imagesLab" + DateTime.Now.GetHashCode() + @"\";
+        public static readonly string BackupTempFolder = AggregatedBackupTempFolder + "pptlabs_imagesLab" 
+            + DateTime.Now.GetHashCode() + @"\";
 
         public static readonly string LoadingImgPath = TempFolder + "loading_" + DateTime.Now.GetHashCode();
         public static readonly string LoadMoreImgPath = TempFolder + "loadMore_" + DateTime.Now.GetHashCode();
@@ -24,6 +30,7 @@ namespace PowerPointLabs.ImageSearch.Util
 
         private static bool Init()
         {
+            Empty(new DirectoryInfo(AggregatedTempFolder));
             if (!Directory.Exists(TempFolder))
             {
                 try
@@ -42,6 +49,7 @@ namespace PowerPointLabs.ImageSearch.Util
         private static bool RetryInit()
         {
             TempFolder = BackupTempFolder;
+            Empty(new DirectoryInfo(AggregatedBackupTempFolder));
             if (!Directory.Exists(TempFolder))
             {
                 try
@@ -77,6 +85,19 @@ namespace PowerPointLabs.ImageSearch.Util
                                     + Guid.NewGuid().ToString().Substring(0, 6)
                                     + DateTime.Now.GetHashCode();
             return fullsizeImageFile;
+        }
+
+        public static void Empty(DirectoryInfo directory)
+        {
+            try
+            {
+                foreach (FileInfo file in directory.GetFiles()) file.Delete();
+                foreach (DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+            }
+            catch (Exception)
+            {
+                // ignore ex, if cannot delete trash
+            }
         }
     }
 }
