425d84d1cb997f3c98e0d8f23480fa6030b297a6
Kai <gigikie@gmail.com>
2016-01-10 16:01:33 +0800

    added ut for ps lab model factory

---
+++ PowerPointLabs/Test/UnitTest/PictureSlidesLab/ModelFactory/StyleOptionsFactoryTest.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerPointLabs;
+using PowerPointLabs.PictureSlidesLab.ModelFactory;
+
+namespace Test.UnitTest.PictureSlidesLab.ModelFactory
+{
+    [TestClass]
+    public class StyleOptionsFactoryTest
+    {
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetAllVariationStyleOptions()
+        {
+            var allOptions = StyleOptionsFactory.GetAllStylesVariationOptions();
+            Assert.IsTrue(allOptions.Count > 0);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetAllPreviewStyleOptions()
+        {
+            var allOptions = StyleOptionsFactory.GetAllStylesPreviewOptions();
+            Assert.IsTrue(allOptions.Count > 0);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetDirectTextOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameDirectText);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameDirectText, option.StyleName);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameDirectText);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetBlurOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameBlur);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameBlur, option.StyleName);
+            Assert.IsTrue(option.IsUseBlurStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameBlur);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetTextBoxOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameTextBox);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameTextBox, option.StyleName);
+            Assert.IsTrue(option.IsUseTextBoxStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameTextBox);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetBannerOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameBanner);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameBanner, option.StyleName);
+            Assert.IsTrue(option.IsUseBannerStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameBanner);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetSpecialEffectOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameSpecialEffect);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameSpecialEffect, option.StyleName);
+            Assert.IsTrue(option.IsUseSpecialEffectStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameSpecialEffect);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetOverlayOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameOverlay);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameOverlay, option.StyleName);
+            Assert.IsTrue(option.IsUseOverlayStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameOverlay);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetOutlineOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameOutline);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameOutline, option.StyleName);
+            Assert.IsTrue(option.IsUseOutlineStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameOutline);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetFrameOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameFrame);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameFrame, option.StyleName);
+            Assert.IsTrue(option.IsUseFrameStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameFrame);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetCircleOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameCircle);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameCircle, option.StyleName);
+            Assert.IsTrue(option.IsUseCircleStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameCircle);
+            Assert.AreEqual(8, options.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetTriangleOptions()
+        {
+            var option = StyleOptionsFactory.GetStylesPreviewOption(
+                TextCollection.PictureSlidesLabText.StyleNameTriangle);
+            Assert.AreEqual(TextCollection.PictureSlidesLabText.StyleNameTriangle, option.StyleName);
+            Assert.IsTrue(option.IsUseTriangleStyle);
+
+            var options = StyleOptionsFactory.GetStylesVariationOptions(
+                TextCollection.PictureSlidesLabText.StyleNameTriangle);
+            Assert.AreEqual(8, options.Count);
+        }
+    }
+}
---
+++ PowerPointLabs/Test/UnitTest/PictureSlidesLab/ModelFactory/StyleVariantsFactoryTest.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerPointLabs;
+using PowerPointLabs.PictureSlidesLab.ModelFactory;
+
+namespace Test.UnitTest.PictureSlidesLab.ModelFactory
+{
+    [TestClass]
+    public class StyleVariantsFactoryTest
+    {
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetDirectTextVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameDirectText);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetBlurVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameBlur);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetTextBoxVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameTextBox);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetBannerVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameBanner);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetSpecialEffectVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameSpecialEffect);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetOverlayVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameOverlay);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetOutlineVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameOutline);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetFrameVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameFrame);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetCircleVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameCircle);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestGetTriangleVariants()
+        {
+            VerifyVariants(TextCollection.PictureSlidesLabText.StyleNameTriangle);
+        }
+
+        private static void VerifyVariants(string styleName)
+        {
+            var variants =
+                StyleVariantsFactory.GetVariants(styleName);
+            var options =
+                StyleOptionsFactory.GetStylesPreviewOption(styleName);
+
+            var numberOfNoEffectVariant = 0;
+            foreach (var variant in variants.Values)
+            {
+                Assert.AreEqual(8, variant.Count,
+                    "Each variant/category/aspect/dimension should have 8 variations");
+                foreach (var styleVariants in variant)
+                {
+                    if (styleVariants.IsNoEffect(options))
+                    {
+                        numberOfNoEffectVariant++;
+                    }
+                }
+            }
+            Assert.AreEqual(variants.Values.Count, numberOfNoEffectVariant,
+                "In order to swap no effect variant with the style correctly, it is assumed that " +
+                "number of no effect variant should be equal to number of variants/category/aspect/dimension. " +
+                "Please modify a variation to have no effect on the style. Ref: issue #802.");
+        }
+    }
+}
