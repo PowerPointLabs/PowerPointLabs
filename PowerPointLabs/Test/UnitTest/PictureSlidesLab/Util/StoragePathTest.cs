d6f75796f99badfda5d71acf3aef8c8c76bb410d
Kai <gigikie@gmail.com>
2016-01-12 22:05:44 +0800

    add utils unit test

---
+++ PowerPointLabs/Test/UnitTest/PictureSlidesLab/Util/StoragePathTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerPointLabs.PictureSlidesLab.Util;
+
+namespace Test.UnitTest.PictureSlidesLab.Util
+{
+    [TestClass]
+    public class StoragePathTest
+    {
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestStoragePathInit()
+        {
+            try
+            {
+                StoragePath.GetPath("some-name");
+                Assert.Fail();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestStoragePathInit2()
+        {
+            try
+            {
+                StoragePath.InitPersistentFolder(new List<string>());
+                StoragePath.GetPath("some-name");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
