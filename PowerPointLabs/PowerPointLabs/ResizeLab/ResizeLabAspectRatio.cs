8d29856347de9b48e8cb9a9000dc2ec592803649
Yu Ting <youthinkk@outlook.com>
2016-01-31 11:23:38 +0800

    Add skeleton of viewmodel of resize lab

---
+++ PowerPointLabs/PowerPointLabs/ResizeLab/ResizeLabAspectRatio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PowerPointLabs.ResizeLab
+{
+    internal static partial class ResizeLabMain
+    {
+    }
+}
