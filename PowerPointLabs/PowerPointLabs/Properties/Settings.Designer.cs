4f4c7e043ae33e138f43c40648f547038e7df768
Kai <gigikie@gmail.com>
2016-02-29 01:17:53 +0800

    dev release 2.8.6.2

--- PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace PowerPointLabs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.8.6.1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.8.6.2")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
