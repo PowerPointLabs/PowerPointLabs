e40117c21df3bf727fe3794ce7f3ae0c26d07377
Victor Hazali <victorhazali@gmail.com>
2016-03-03 17:52:49 +0800

    Changed settings icon Fixes #23

--- PowerPointLabs/PowerPointLabs/Properties/Resources.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Resources.Designer.cs
@@ -973,6 +973,16 @@ namespace PowerPointLabs.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap SettingsGear {
+            get {
+                object obj = ResourceManager.GetObject("SettingsGear", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap ShapesLab {
             get {
                 object obj = ResourceManager.GetObject("ShapesLab", resourceCulture);
