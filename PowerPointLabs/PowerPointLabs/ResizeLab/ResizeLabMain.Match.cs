a154156a067f42d07dfdf0215a9fc331b1e3e8f9
Yu Ting <youthinkk@outlook.com>
2016-03-24 01:53:08 +0800

    Implement MATCH function

---
+++ PowerPointLabs/PowerPointLabs/ResizeLab/ResizeLabMain.Match.cs
@@ -0,0 +1,42 @@
+﻿using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace PowerPointLabs.ResizeLab
+{
+    partial class ResizeLabMain
+    {
+        // To be used for error handling
+        internal const int Match_MinNoOfShapesRequired = 1;
+        internal const string Match_FeatureName = "Match";
+        internal const string Match_ShapeSupport = "object";
+        internal static readonly string[] Match_ErrorParameters =
+        {
+            Match_FeatureName,
+            Match_MinNoOfShapesRequired.ToString(),
+            Match_ShapeSupport
+        };
+
+        /// <summary>
+        /// Resize the height of selected shapes to match their width.
+        /// </summary>
+        /// <param name="selectedShapes"></param>
+        public void MatchWidth(PowerPoint.ShapeRange selectedShapes)
+        {
+            foreach (PowerPoint.Shape shape in selectedShapes)
+            {
+                shape.Height = shape.Width;
+            }
+        }
+
+        /// <summary>
+        /// Resize the width of selected shapes to match their height.
+        /// </summary>
+        /// <param name="selectedShapes"></param>
+        public void MatchHeight(PowerPoint.ShapeRange selectedShapes)
+        {
+            foreach (PowerPoint.Shape shape in selectedShapes)
+            {
+                shape.Width = shape.Height;
+            }
+        }
+    }
+}
