ed2dee4e163ef77f33ee5ad0aad8ce7c79c22c73
Yu Ting <youthinkk@outlook.com>
2016-03-28 01:41:14 +0800

    Implement unit test for MATCH

--- PowerPointLabs/Test/UnitTest/ResizeLab/MainSettingsTest.cs
+++ PowerPointLabs/Test/UnitTest/ResizeLab/MainSettingsTest.cs
@@ -15,7 +15,7 @@ namespace Test.UnitTest.ResizeLab
         private const string WithAspectRatioShapeNames = "withAspectRatio";
         private const string WithoutAspectRatioShapeNames = "withoutAspectRatio";
 
-        private const int AspectRatioSlideNo = 24;
+        private const int AspectRatioSlideNo = 29;
 
         [TestInitialize]
         public void TestInitialize()
