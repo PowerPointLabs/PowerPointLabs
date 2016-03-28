6154511aedd77b817ac048c44525c5a227e710c2
Yu Ting <youthinkk@outlook.com>
2016-03-29 00:23:26 +0800

    Implement checking of input resize factor

--- PowerPointLabs/PowerPointLabs/ResizeLab/ResizeLabPaneWPF.xaml.cs
+++ PowerPointLabs/PowerPointLabs/ResizeLab/ResizeLabPaneWPF.xaml.cs
@@ -451,6 +451,34 @@ namespace PowerPointLabs.ResizeLab
             return this.GetCurrentSelection();
         }
 
+        private float? ConvertToFloat(string input)
+        {
+            try
+            {
+                return float.Parse(input);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private bool IsValidFactor(float? factor)
+        {
+            try
+            {
+                if (factor > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return false;
+        }
+
         private void ClickHandler(Action<PowerPoint.ShapeRange> resizeAction, int minNoOfSelectedShapes, string[] errorParameters)
         {
             var selectedShapes = GetSelectedShapes();
