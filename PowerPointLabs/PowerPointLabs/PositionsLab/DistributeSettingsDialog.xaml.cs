23fd63d5a768c3407c4048c230a8d4b2b9934e45
Oswell <oswellchan@gmail.com>
2016-04-05 16:56:33 +0800

    Shift grid related settings into grid dialog

--- PowerPointLabs/PowerPointLabs/PositionsLab/DistributeSettingsDialog.xaml.cs
+++ PowerPointLabs/PowerPointLabs/PositionsLab/DistributeSettingsDialog.xaml.cs
@@ -17,50 +17,6 @@ namespace PowerPointLabs.PositionsLab
         }
 
         #region On-Load Settings
-        private void MarginTopInput_Load(object sender, RoutedEventArgs e)
-        {
-            marginTopInput.Value = PositionsLabMain.MarginTop;
-        }
-
-        private void MarginBottomInput_Load(object sender, RoutedEventArgs e)
-        {
-            marginBottomInput.Value = PositionsLabMain.MarginBottom;
-        }
-
-        private void MarginLeftInput_Load(object sender, RoutedEventArgs e)
-        {
-            marginLeftInput.Value = PositionsLabMain.MarginLeft;
-        }
-
-        private void MarginRightInput_Load(object sender, RoutedEventArgs e)
-        {
-            marginRightInput.Value = PositionsLabMain.MarginRight;
-        }
-
-        private void AlignLeftButton_Load(object sender, RoutedEventArgs e)
-        {
-            if (PositionsLabMain.DistributeGridAlignment == PositionsLabMain.GridAlignment.AlignLeft)
-            {
-                alignLeftButton.IsChecked = true;
-            }
-        }
-
-        private void AlignCenterButton_Load(object sender, RoutedEventArgs e)
-        {
-            if (PositionsLabMain.DistributeGridAlignment == PositionsLabMain.GridAlignment.AlignCenter)
-            {
-                alignCenterButton.IsChecked = true;
-            }
-        }
-
-        private void AlignRightButton_Load(object sender, RoutedEventArgs e)
-        {
-            if (PositionsLabMain.DistributeGridAlignment == PositionsLabMain.GridAlignment.AlignRight)
-            {
-                alignRightButton.IsChecked = true;
-            }
-        }
-
         private void DistributeToSlideButton_Load(object sender, RoutedEventArgs e)
         {
             if (PositionsLabMain.DistributeReference == PositionsLabMain.DistributeReferenceObject.Slide)
@@ -113,11 +69,6 @@ namespace PowerPointLabs.PositionsLab
         #region Button actions
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
-            var marginTopValue = marginTopInput.Value;
-            var marginBottomValue = marginBottomInput.Value;
-            var marginLeftValue = marginLeftInput.Value;
-            var marginRightValue = marginRightInput.Value;
-
             // Checks for boundary reference
             if (distributeToSlideButton.IsChecked.GetValueOrDefault())
             {
@@ -148,37 +99,6 @@ namespace PowerPointLabs.PositionsLab
             if (distributeByShapeCenterButton.IsChecked.GetValueOrDefault())
             {
                 PositionsLabMain.DistributeSpaceByCenter();
-            }
-
-            // Checks for margin values
-            if (!marginTopValue.HasValue || marginTopValue.GetValueOrDefault() < 0 ||
-                !marginBottomValue.HasValue || marginBottomValue.GetValueOrDefault() < 0 ||
-                !marginLeftValue.HasValue || marginLeftValue.GetValueOrDefault() < 0 ||
-                !marginRightValue.HasValue || marginRightValue.GetValueOrDefault() < 0)
-            {
-                // TODO: Notify the user that not successfully changed
-                return;
-            }
-
-            PositionsLabMain.SetDistributeMarginTop((float)marginTopValue);
-            PositionsLabMain.SetDistributeMarginBottom((float)marginBottomValue);
-            PositionsLabMain.SetDistributeMarginLeft((float)marginLeftValue);
-            PositionsLabMain.SetDistributeMarginRight((float)marginRightValue);
-
-            // Checks for distribute grid align reference
-            if (alignLeftButton.IsChecked.GetValueOrDefault())
-            {
-                PositionsLabMain.SetDistributeGridAlignment(PositionsLabMain.GridAlignment.AlignLeft);
-            }
-
-            if (alignCenterButton.IsChecked.GetValueOrDefault())
-            {
-                PositionsLabMain.SetDistributeGridAlignment(PositionsLabMain.GridAlignment.AlignCenter);
-            }
-
-            if (alignRightButton.IsChecked.GetValueOrDefault())
-            {
-                PositionsLabMain.SetDistributeGridAlignment(PositionsLabMain.GridAlignment.AlignRight);
             }
 
             IsOpen = false;
--- PowerPointLabs/PowerPointLabs/PositionsLab/PositionsDistributeGridDialog.xaml.cs
+++ PowerPointLabs/PowerPointLabs/PositionsLab/PositionsDistributeGridDialog.xaml.cs
@@ -49,6 +49,51 @@ namespace PowerPointLabs.PositionsLab
         {
             colInput.Value = _rowLength;
         }
+
+        private void MarginTopInput_Load(object sender, RoutedEventArgs e)
+        {
+            marginTopInput.Value = PositionsLabMain.MarginTop;
+        }
+
+        private void MarginBottomInput_Load(object sender, RoutedEventArgs e)
+        {
+            marginBottomInput.Value = PositionsLabMain.MarginBottom;
+        }
+
+        private void MarginLeftInput_Load(object sender, RoutedEventArgs e)
+        {
+            marginLeftInput.Value = PositionsLabMain.MarginLeft;
+        }
+
+        private void MarginRightInput_Load(object sender, RoutedEventArgs e)
+        {
+            marginRightInput.Value = PositionsLabMain.MarginRight;
+        }
+
+        private void AlignLeftButton_Load(object sender, RoutedEventArgs e)
+        {
+            if (PositionsLabMain.DistributeGridAlignment == PositionsLabMain.GridAlignment.AlignLeft)
+            {
+                alignLeftButton.IsChecked = true;
+            }
+        }
+
+        private void AlignCenterButton_Load(object sender, RoutedEventArgs e)
+        {
+            if (PositionsLabMain.DistributeGridAlignment == PositionsLabMain.GridAlignment.AlignCenter)
+            {
+                alignCenterButton.IsChecked = true;
+            }
+        }
+
+        private void AlignRightButton_Load(object sender, RoutedEventArgs e)
+        {
+            if (PositionsLabMain.DistributeGridAlignment == PositionsLabMain.GridAlignment.AlignRight)
+            {
+                alignRightButton.IsChecked = true;
+            }
+        }
+
         private void RowInput_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double?> e)
         {
             if (colInput == null || rowInput == null)
@@ -90,8 +135,44 @@ namespace PowerPointLabs.PositionsLab
             IsOpen = false;
         }
 
-        private void OKButton_Click(object sender, RoutedEventArgs e)
+        private void OkButton_Click(object sender, RoutedEventArgs e)
         {
+            var marginTopValue = marginTopInput.Value;
+            var marginBottomValue = marginBottomInput.Value;
+            var marginLeftValue = marginLeftInput.Value;
+            var marginRightValue = marginRightInput.Value;
+
+            // Checks for margin values
+            if (!marginTopValue.HasValue || marginTopValue.GetValueOrDefault() < 0 ||
+                !marginBottomValue.HasValue || marginBottomValue.GetValueOrDefault() < 0 ||
+                !marginLeftValue.HasValue || marginLeftValue.GetValueOrDefault() < 0 ||
+                !marginRightValue.HasValue || marginRightValue.GetValueOrDefault() < 0)
+            {
+                // TODO: Notify the user that not successfully changed
+                return;
+            }
+
+            PositionsLabMain.SetDistributeMarginTop((float)marginTopValue);
+            PositionsLabMain.SetDistributeMarginBottom((float)marginBottomValue);
+            PositionsLabMain.SetDistributeMarginLeft((float)marginLeftValue);
+            PositionsLabMain.SetDistributeMarginRight((float)marginRightValue);
+
+            // Checks for distribute grid align reference
+            if (alignLeftButton.IsChecked.GetValueOrDefault())
+            {
+                PositionsLabMain.SetDistributeGridAlignment(PositionsLabMain.GridAlignment.AlignLeft);
+            }
+
+            if (alignCenterButton.IsChecked.GetValueOrDefault())
+            {
+                PositionsLabMain.SetDistributeGridAlignment(PositionsLabMain.GridAlignment.AlignCenter);
+            }
+
+            if (alignRightButton.IsChecked.GetValueOrDefault())
+            {
+                PositionsLabMain.SetDistributeGridAlignment(PositionsLabMain.GridAlignment.AlignRight);
+            }
+
             ShapeRange simulatedShapes = null;
             try
             {
@@ -130,6 +211,7 @@ namespace PowerPointLabs.PositionsLab
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
+            IsOpen = false;
             Close();
         }
 
