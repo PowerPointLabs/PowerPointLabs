13f406f8fe878496df02397e0ea977f792bc59d8
Yu Ting <youthinkk@outlook.com>
2016-04-06 06:33:33 +0800

    Implement resize actual dimension in MATCH

--- PowerPointLabs/PowerPointLabs/ResizeLab/ResizeLabMain.Match.cs
+++ PowerPointLabs/PowerPointLabs/ResizeLab/ResizeLabMain.Match.cs
@@ -30,6 +30,40 @@ namespace PowerPointLabs.ResizeLab
         /// <param name="selectedShapes"></param>
         public void MatchWidth(PowerPoint.ShapeRange selectedShapes)
         {
+            switch (ResizeType)
+            {
+                case ResizeBy.Visual:
+                    MatchVisualWidth(selectedShapes);
+                    break;
+                case ResizeBy.Actual:
+                    MatchActualWidth(selectedShapes);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Resize the width of selected shapes to match their height.
+        /// </summary>
+        /// <param name="selectedShapes"></param>
+        public void MatchHeight(PowerPoint.ShapeRange selectedShapes)
+        {
+            switch (ResizeType)
+            {
+                case ResizeBy.Visual:
+                    MatchVisualHeight(selectedShapes);
+                    break;
+                case ResizeBy.Actual:
+                    MatchActualHeight(selectedShapes);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Resize the visual height of selected shapes to match their visual width.
+        /// </summary>
+        /// <param name="selectedShapes"></param>
+        private void MatchVisualWidth(PowerPoint.ShapeRange selectedShapes)
+        {
             try
             {
                 var isAspectRatio = selectedShapes.LockAspectRatio;
@@ -48,15 +82,15 @@ namespace PowerPointLabs.ResizeLab
             }
             catch (Exception e)
             {
-                Logger.LogException(e, "MatchWidth");
+                Logger.LogException(e, "MatchVisualWidth");
             }
         }
 
         /// <summary>
-        /// Resize the width of selected shapes to match their height.
+        /// Resize the visual width of selected shapes to match their visual height.
         /// </summary>
         /// <param name="selectedShapes"></param>
-        public void MatchHeight(PowerPoint.ShapeRange selectedShapes)
+        private void MatchVisualHeight(PowerPoint.ShapeRange selectedShapes)
         {
             try
             {
@@ -76,7 +110,63 @@ namespace PowerPointLabs.ResizeLab
             }
             catch (Exception e)
             {
-                Logger.LogException(e, "MatchHeight");
+                Logger.LogException(e, "MatchVisualHeight");
+            }
+        }
+
+        /// <summary>
+        /// Resize the actual height of selected shapes to match their actual width.
+        /// </summary>
+        /// <param name="selectedShapes"></param>
+        private void MatchActualWidth(PowerPoint.ShapeRange selectedShapes)
+        {
+            try
+            {
+                var isAspectRatio = selectedShapes.LockAspectRatio;
+                selectedShapes.LockAspectRatio = MsoTriState.msoFalse;
+
+                for (int i = 1; i <= selectedShapes.Count; i++)
+                {
+                    var shape = new PPShape(selectedShapes[i], false);
+                    var anchorPoint = GetActualAnchorPoint(shape);
+
+                    shape.ShapeHeight = shape.ShapeWidth;
+                    AlignActualShape(shape, anchorPoint);
+                }
+
+                selectedShapes.LockAspectRatio = isAspectRatio;
+            }
+            catch (Exception e)
+            {
+                Logger.LogException(e, "MatchActualWidth");
+            }
+        }
+
+        /// <summary>
+        /// Resize the actual width of selected shapes to match their actual height.
+        /// </summary>
+        /// <param name="selectedShapes"></param>
+        private void MatchActualHeight(PowerPoint.ShapeRange selectedShapes)
+        {
+            try
+            {
+                var isAspectRatio = selectedShapes.LockAspectRatio;
+                selectedShapes.LockAspectRatio = MsoTriState.msoFalse;
+
+                for (int i = 1; i <= selectedShapes.Count; i++)
+                {
+                    var shape = new PPShape(selectedShapes[i], false);
+                    var anchorPoint = GetActualAnchorPoint(shape);
+
+                    shape.ShapeWidth = shape.ShapeHeight;
+                    AlignActualShape(shape, anchorPoint);
+                }
+
+                selectedShapes.LockAspectRatio = isAspectRatio;
+            }
+            catch (Exception e)
+            {
+                Logger.LogException(e, "MatchActualHeight");
             }
         }
     }
