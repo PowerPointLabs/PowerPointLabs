1cfaf72d90037426c87d54c1552893824738d8c9
Dawnlord <290233090lhy@gmail.com>
2016-06-06 10:17:51 +0800

    Skeleton of UT Test

---
+++ PowerPointLabs/Test/UnitTest/PositionsLab/PositionsLabDuplicateAndRotateTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PowerPointLabs.PositionsLab;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using PowerPointLabs.Utils;
+using System.Diagnostics;
+
+namespace Test.UnitTest.PositionsLab
+{
+    [TestClass]
+    public class PositionsLabDuplicateAndRotateTest : BasePositionsLabTest
+    {
+        private List<string> _shapeNames;
+
+        private const int OriginalShapesSlideNo = 3;
+
+        protected override string GetTestingSlideName()
+        {
+            return "PositionsLab\\PositionsLabDuplicateAndRotate.pptx";
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            PositionsLabMain.InitPositionsLab();
+
+            _shapeNames = new List<string> { };
+            InitOriginalShapes(OriginalShapesSlideNo, _shapeNames);
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            RestoreShapes(OriginalShapesSlideNo, _shapeNames);
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestDuplicateAndRoate()
+        {
+        }
+
+        [TestMethod]
+        [TestCategory("UT")]
+        public void TestDuplcateAndRoate2()
+        {
+        }
+    }
+}
