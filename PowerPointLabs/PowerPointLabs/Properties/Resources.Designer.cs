75b1f5c9ff319c648569583268749104f0db322f
fionachang <fiona.chang.w@gmail.com>
2016-06-22 15:42:31 +0800

    Add icon

--- PowerPointLabs/PowerPointLabs/Properties/Resources.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Resources.Designer.cs
@@ -523,6 +523,16 @@ namespace PowerPointLabs.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap DistributeAngleIcon {
+            get {
+                object obj = ResourceManager.GetObject("DistributeAngleIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap DistributeCenterIcon {
             get {
                 object obj = ResourceManager.GetObject("DistributeCenterIcon", resourceCulture);
