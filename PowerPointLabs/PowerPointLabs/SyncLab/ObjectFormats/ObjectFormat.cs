8d5b80fb8818ace77a32bc8ba9bbcdd99bdf1eb2
Ken Loh <Kenneth_LJS@Live.com>
2017-01-26 13:17:07 +0800

    Add Sync Lab infrastructure

---
+++ PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/ObjectFormat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace PowerPointLabs.SyncLab
+{
+    public abstract class ObjectFormat
+    {
+
+        public static readonly Size DISPLAY_IMAGE_SIZE = new Size(32, 32);
+
+        protected string displayText;
+        protected Image displayImage;
+
+        public abstract void ApplyTo(Shape shape);
+
+        public string DisplayText
+        {
+            get
+            {
+                return displayText;
+            }
+        }
+
+        public Image DisplayImage
+        {
+            get
+            {
+                return displayImage;
+            }
+        }
+    }
+}
