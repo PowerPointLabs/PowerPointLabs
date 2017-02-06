3d807dd424392aab79d839b670ae20d82328a8af
Ang Shi Ya <shiya_95@hotmail.com>
2017-02-06 22:58:58 +0800

    Effects Lab: Blur/recolor all except selected changes original slide #1108 (#1112)
    * Replace edited orignal slide with duplicate
    
    * Add test cases

--- PowerPointLabs/Test/FunctionalTest/EffectsLabTest.cs
+++ PowerPointLabs/Test/FunctionalTest/EffectsLabTest.cs
@@ -17,6 +17,8 @@ namespace Test.FunctionalTest
         public void FT_EffectsLabTest()
         {
             PplFeatures.BlurrinessOverlay("EffectsLabBlurBackground", true);
+            TestRemainderEffect(46, PplFeatures.SepiaBackgroundEffect);
+            TestRemainderEffect(43, PplFeatures.BlurBackgroundEffect);
             TestRemainderEffect(40, PplFeatures.BlurBackgroundEffect);
             PplFeatures.BlurrinessOverlay("EffectsLabBlurRemainder", true);
             TestRemainderEffect(37, PplFeatures.BlurRemainderEffect);
