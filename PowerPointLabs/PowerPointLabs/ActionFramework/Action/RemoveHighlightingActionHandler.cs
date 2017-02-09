5e1b5331ce0bec9c214d842a45f889c361160256
Jamos Tay <jamostay94@hotmail.com>
2017-02-09 13:16:21 +0800

    Make changes for review

--- PowerPointLabs/PowerPointLabs/ActionFramework/Action/RemoveHighlightingActionHandler.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Action/RemoveHighlightingActionHandler.cs
@@ -2,6 +2,7 @@
 using PowerPointLabs.ActionFramework.Common.Attribute;
 using PowerPointLabs.ActionFramework.Common.Extension;
 using PowerPointLabs.ActionFramework.Common.Interface;
+using Microsoft.Office.Interop.PowerPoint;
 
 namespace PowerPointLabs.ActionFramework.Action
 {
