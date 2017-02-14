a3b057d7d721ac31f5b6cb02a31952c99a47351b
Syed <syed@syedabdullah.net>
2017-02-15 00:34:42 +0800

    Core Files and Paste to Fill Slide (#1137)

---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Action/PasteLab/PasteLabFillActionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Extension;
+using PowerPointLabs.ActionFramework.Common.Interface;
+
+namespace PowerPointLabs.ActionFramework.Action.PasteLab
+{
+    [ExportActionRibbonId("pasteToFillSlide")]
+    class PasteLabFillActionHandler : ActionHandler
+    {
+        protected override void ExecuteAction(string ribbonId)
+        {
+            var curPresentation = this.GetCurrentPresentation();
+            bool clipboardIsEmpty = (Clipboard.GetDataObject() == null);
+            PowerPointLabs.PasteLab.PasteLabMain.PasteToFillSlide(this.GetCurrentSlide(), clipboardIsEmpty, curPresentation.SlideWidth, curPresentation.SlideHeight);
+        }
+    }
+}
---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Label/PasteLab/PasteLabFillLabelHandler.cs
@@ -0,0 +1,14 @@
+﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Interface;
+
+namespace PowerPointLabs.ActionFramework.Label.PasteLab
+{
+    [ExportLabelRibbonId("pasteToFillSlide")]
+    class PasteLabFillLabelHandler : LabelHandler
+    {
+        protected override string GetLabel(string ribbonId)
+        {
+            return TextCollection.PasteLabText.PasteToFillSlide;
+        }
+    }
+}
