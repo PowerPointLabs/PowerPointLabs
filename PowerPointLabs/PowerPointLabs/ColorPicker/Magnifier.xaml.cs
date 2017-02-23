76df51ea9a22711f6ac280483266c4d53b38e1f8
initialshl <initialshl@outlook.com>
2017-02-24 00:53:25 +0800

    Convert Magnifier to WPF

--- PowerPointLabs/PowerPointLabs/ColorPicker/Magnifier.xaml.cs
+++ PowerPointLabs/PowerPointLabs/ColorPicker/Magnifier.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 
@@ -13,38 +14,108 @@ namespace PowerPointLabs.ColorPicker
     /// </summary>
     public partial class Magnifier : Window
     {
-        private MagnifierOverlay magnifierOverlay;
+        private MagnificationControlHost magnificationControl;
         private System.Windows.Forms.Timer timer;
+
         private IntPtr hwndMag;
-        private List<IntPtr> magFilteredWindows;
         private bool isMagInitialized;
+        
         private float magnificationFactor;
+        private Size magnifierHalfSize;
         private Size sourceSize;
         private Size sourceHalfSize;
-        private Size actualHalfSize;
 
         public Magnifier(float magnificationFactor)
         {
             InitializeComponent();
 
-            // Calculate dimensions once
-            this.magnificationFactor = magnificationFactor;
-            sourceSize.Width = (int)(Width / magnificationFactor);
-            sourceSize.Height = (int)(Height / magnificationFactor);
-            sourceHalfSize.Width = sourceSize.Width / 2;
-            sourceHalfSize.Height = sourceSize.Height / 2;
-            actualHalfSize.Width = Width / 2;
-            actualHalfSize.Height = Height / 2;
-
             timer = new System.Windows.Forms.Timer();
             timer.Interval = 100;
             timer.Tick += new EventHandler(Timer_Tick);
 
-            magnifierOverlay = new MagnifierOverlay();
-            magnifierOverlay.Loaded += MagnifierOverlay_Loaded;
-
+            this.magnificationFactor = magnificationFactor;
             Visibility = Visibility.Visible;
         }
+
+        #region Magnification HwndHost Win32 Interop
+
+        private class MagnificationControlHost : HwndHost
+        {
+            IntPtr hwndControl;
+            IntPtr hwndHost;
+            int hostWidth, hostHeight;
+
+            internal const int HOST_ID = 0x00000002;
+            internal const string WC_MAGNIFIER = "Magnifier";
+
+            public MagnificationControlHost(double width, double height)
+            {
+                hostWidth = (int)(width * Utils.Graphics.GetDpiScale());
+                hostHeight = (int)(height * Utils.Graphics.GetDpiScale());
+            }
+
+            protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+            {
+                hwndControl = IntPtr.Zero;
+                hwndHost = IntPtr.Zero;
+
+                hwndHost = Native.CreateWindowEx((int)Native.ExtendedWindowStyles.WS_EX_LAYERED |
+                                                (int)Native.ExtendedWindowStyles.WS_EX_TRANSPARENT, 
+                                                "static", "",
+                                                (int)Native.WindowStyles.WS_CHILD |
+                                                (int)Native.WindowStyles.WS_VISIBLE,
+                                                0, 0,
+                                                hostWidth, hostHeight,
+                                                hwndParent.Handle,
+                                                (IntPtr)HOST_ID,
+                                                IntPtr.Zero,
+                                                0);
+                Native.SetLayeredWindowAttributes(hwndHost, 0, 255, Native.LayeredWindowAttributeFlags.LWA_ALPHA);
+
+                // Create Magnification control
+                hwndControl = Native.CreateWindowEx(0, WC_MAGNIFIER, "MagnificationControl",
+                                                (int)Native.WindowStyles.WS_CHILD |
+                                                (int)Native.WindowStyles.WS_VISIBLE |
+                                                (int)Native.MagnifierStyle.MS_CLIPAROUNDCURSOR,
+                                                0, 0,
+                                                hostWidth, hostHeight,
+                                                hwndHost, 
+                                                IntPtr.Zero, 
+                                                IntPtr.Zero, 
+                                                0);
+
+                // Clip window into ellipse
+                IntPtr ellipseRegion = Native.CreateRoundRectRgn(0, 0,
+                                                hostWidth, hostHeight,
+                                                hostWidth, hostHeight);
+                Native.SetWindowRgn(hwndHost, ellipseRegion, true);
+
+                // Make window click-through
+                int extendedStyle = Native.GetWindowLong(hwndParent.Handle, (int)Native.WindowLong.GWL_EXSTYLE);
+                Native.SetWindowLong(hwndParent.Handle, (int)Native.WindowLong.GWL_EXSTYLE, 
+                                                extendedStyle | (int)Native.ExtendedWindowStyles.WS_EX_TRANSPARENT);
+
+                return new HandleRef(this, hwndHost);
+            }
+
+            protected override IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+            {
+                handled = false;
+                return IntPtr.Zero;
+            }
+
+            protected override void DestroyWindowCore(HandleRef hwnd)
+            {
+                Native.DestroyWindow(hwnd.Handle);
+            }
+
+            public IntPtr HwndMagnification
+            {
+                get { return hwndControl; }
+            }
+        }
+
+        #endregion
 
         #region Public API
 
@@ -54,7 +125,6 @@ namespace PowerPointLabs.ColorPicker
             {
                 UpdateMagnifier();
                 timer.Start();
-                magnifierOverlay.Show();
                 Visibility = Visibility.Visible;
             }
         }
@@ -62,7 +132,6 @@ namespace PowerPointLabs.ColorPicker
         public new void Hide()
         {
             timer.Stop();
-            magnifierOverlay.Hide();
             Visibility = Visibility.Collapsed;
         }
 
@@ -74,7 +143,8 @@ namespace PowerPointLabs.ColorPicker
         {
             try
             {
-                if (isMagInitialized = Native.MagInitialize())
+                isMagInitialized = Native.MagInitialize();
+                if (isMagInitialized)
                 {
                     SetupMagnifier();
                 }
@@ -85,20 +155,17 @@ namespace PowerPointLabs.ColorPicker
                 Logger.LogException(exception, "Magnifier_Window_Loaded");
                 TeardownMagnifier();
                 isMagInitialized = false;
-                return;
+            }
+            finally
+            {
+                // Hide after initializing UI
+                Hide();
             }
         }
 
         private void Window_Unloaded(object sender, RoutedEventArgs e)
         {
             TeardownMagnifier();
-        }
-
-        private void MagnifierOverlay_Loaded(object sender, EventArgs e)
-        {
-            // Overlay handle is only created after it is loaded
-            IntPtr overlayHwnd = new WindowInteropHelper(magnifierOverlay).Handle;
-            AddMagnifierFilteredWindow(overlayHwnd);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -112,19 +179,17 @@ namespace PowerPointLabs.ColorPicker
 
         private void SetupMagnifier()
         {
-            magFilteredWindows = new List<IntPtr>();
-            IntPtr hInst = Native.GetModuleHandle(null);
-            IntPtr hwnd = new WindowInteropHelper(this).Handle;
-
-            // Create Magnifier window
-            hwndMag = Native.CreateWindowEx(
-                (int)Native.ExtendedWindowStyles.WS_EX_CLIENTEDGE,
-                Native.WC_MAGNIFIER, "MagnifierWindow",
-                (int)Native.WindowStyles.WS_CHILD |
-                (int)Native.WindowStyles.WS_VISIBLE |
-                (int)Native.MagnifierStyle.MS_CLIPAROUNDCURSOR,
-                0, 0, (int)Width, (int)Height,
-                hwnd, IntPtr.Zero, hInst, IntPtr.Zero);
+            // Calculate dimensions once
+            magnifierHalfSize.Width = Width / 2;
+            magnifierHalfSize.Height = ActualHeight / 2;
+            sourceSize.Width = (int)(Width / magnificationFactor);
+            sourceSize.Height = (int)(Height / magnificationFactor);
+            sourceHalfSize.Width = sourceSize.Width / 2;
+            sourceHalfSize.Height = sourceSize.Height / 2;
+            
+            magnificationControl = new MagnificationControlHost(Width - OutlineWidth.Width, Height - OutlineWidth.Width);
+            MagnificationHostElement.Child = magnificationControl;
+            hwndMag = magnificationControl.HwndMagnification;
 
             if (hwndMag == IntPtr.Zero)
             {
@@ -140,23 +205,22 @@ namespace PowerPointLabs.ColorPicker
             Native.MagSetWindowTransform(hwndMag, ref magTransformMatrix);
         }
 
-        private void AddMagnifierFilteredWindow(IntPtr handle)
+        private void TeardownMagnifier()
         {
-            magFilteredWindows.Add(handle);
-            Native.MagSetWindowFilterList(hwndMag, (int)Native.MagnifierFilterMode.MW_FILTERMODE_EXCLUDE,
-                                          magFilteredWindows.Count, magFilteredWindows.ToArray());
+            if (isMagInitialized)
+            {
+                Native.MagUninitialize();
+            }
         }
 
         private void UpdateMagnifier()
         {
             System.Drawing.Point mousePosition = System.Windows.Forms.Control.MousePosition;
-            mousePosition.X = (int)(mousePosition.X / Utils.Graphics.GetDpiScale());
-            mousePosition.Y = (int)(mousePosition.Y / Utils.Graphics.GetDpiScale());
-            
+
             // Set the source rectangle for the magnifier control
             Native.RECT sourceRect = new Native.RECT();
-            sourceRect.Left = (int)(mousePosition.X - sourceHalfSize.Width);
-            sourceRect.Top = (int)(mousePosition.Y - sourceHalfSize.Height);
+            sourceRect.Left = (int)(mousePosition.X - sourceSize.Width);
+            sourceRect.Top = (int)(mousePosition.Y - sourceSize.Height);
             sourceRect.Right = sourceRect.Left + (int)sourceSize.Width;
             sourceRect.Bottom = sourceRect.Top + (int)sourceSize.Height;
             Native.MagSetWindowSource(hwndMag, sourceRect);
@@ -164,19 +228,13 @@ namespace PowerPointLabs.ColorPicker
             // Force redraw
             Native.InvalidateRect(hwndMag, IntPtr.Zero, true);
 
-            // Update position
-            Left = mousePosition.X - actualHalfSize.Width;
-            Top = mousePosition.Y - actualHalfSize.Height;
-            magnifierOverlay.Left = mousePosition.X - magnifierOverlay.HalfSize.X;
-            magnifierOverlay.Top = mousePosition.Y - magnifierOverlay.HalfSize.Y;
-        }
+            // WPF units are affected by monitor's DPI
+            mousePosition.X = (int)(mousePosition.X / Utils.Graphics.GetDpiScale());
+            mousePosition.Y = (int)(mousePosition.Y / Utils.Graphics.GetDpiScale());
 
-        private void TeardownMagnifier()
-        {
-            if (isMagInitialized)
-            {
-                Native.MagUninitialize();
-            }
+            // Update position
+            Left = mousePosition.X - magnifierHalfSize.Width;
+            Top = mousePosition.Y - magnifierHalfSize.Height;
         }
 
         #endregion
