7166a0164d61db5cb82a5ac12b583675fe0232fa
Ken Loh <Kenneth_LJS@Live.com>
2017-02-25 15:55:09 +0800

    SyncLab GUI Update

---
+++ PowerPointLabs/PowerPointLabs/SyncLab/SyncFormatConstants.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace PowerPointLabs.SyncLab.ObjectFormats
+{
+    class SyncFormatConstants
+    {
+
+        public static FormatTreeNode[] FormatCategories
+        {
+            get
+            {
+                return new FormatTreeNode[]
+                {
+                    new FormatTreeNode(
+                            "Text",
+                            new FormatTreeNode("Font", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Font Size", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Font Color", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Bold", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Italics", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Underline", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Shadow", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Strikethrough", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Character Spacing", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Line Spacing", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Alignment", new Format(typeof(FillFormat)))
+                        ),
+                    new FormatTreeNode(
+                            "Fill",
+                            new FormatTreeNode("Fill", new Format(typeof(FillFormat)))
+                        ),
+                    new FormatTreeNode(
+                            "Line",
+                            new FormatTreeNode("Start Arrow", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("End Arrow", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Weight", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Style", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Fill", new Format(typeof(FillFormat)))
+                        ),
+                    new FormatTreeNode(
+                            "Effect",
+                            new FormatTreeNode("Shadow", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Reflection", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Glow", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Soft Edge", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Bevel", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("3D Rotation", new Format(typeof(FillFormat)))
+                        ),
+                    new FormatTreeNode(
+                            "Size/Position",
+                            new FormatTreeNode("Width", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Height", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("X", new Format(typeof(FillFormat))),
+                            new FormatTreeNode("Y", new Format(typeof(FillFormat)))
+                        )
+                };
+                /*
+                KeyValuePair<String, Type>[] types = new KeyValuePair<String, Type>[]
+                {
+                    new KeyValuePair<string, Type>("Fill\\Fill Style", typeof(FillFormat)),
+                    new KeyValuePair<string, Type>("Fill\\Fill Style 2", typeof(FillFormat)),
+                    new KeyValuePair<string, Type>("Fill\\Fill Style 3", typeof(FillFormat)),
+                    new KeyValuePair<string, Type>("Fill\\Fill Style 4", typeof(FillFormat)),
+                    new KeyValuePair<string, Type>("Line\\Line Fill", typeof(LineFormat))
+                };
+                KeyValuePair<String, Format>[] categories =
+                        new KeyValuePair<String, Format>[types.Length];
+                HashSet<String> seenCategories = new HashSet<String>();
+                for (int i = 0; i < categories.Length; i++)
+                {
+                    string category = types[i].Key;
+                    Type formatType = types[i].Value;
+                    Debug.Assert(!seenCategories.Contains(types[i].Key), "Duplicate key");
+                    seenCategories.Add(types[i].Key);
+                    categories[i] = new KeyValuePair<string, Format>(
+                            types[i].Key,
+                            new Format(types[i].Value)
+                        );
+                }
+                return categories;
+                */
+                /*
+                FormatCategory[] categories = new FormatCategory[]
+                {
+                    new FormatCategory(
+                        "Text",
+                        new Type[] {
+                        }
+                    ),
+                    new FormatCategory(
+                        "Fill",
+                        new Type[] {
+                            typeof(FillFormat)
+                        }
+                    ),
+                    new FormatCategory(
+                        "Line",
+                        new Type[] {
+                            typeof(LineFormat)
+                        }
+                    ),
+                    new FormatCategory(
+                        "Effect",
+                        new Type[] {
+                        }
+                    ),
+                    new FormatCategory(
+                        "Size/Position",
+                        new Type[] {
+                        }
+                    )
+                };
+                return categories;
+                */
+            }
+        }
+    }
+}
--- PowerPointLabs/PowerPointLabs/SyncLab/View/SyncFormatDialog.xaml.cs
+++ PowerPointLabs/PowerPointLabs/SyncLab/View/SyncFormatDialog.xaml.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using Microsoft.Office.Interop.PowerPoint;
+using PowerPointLabs.SyncLab.ObjectFormats;
 
 namespace PowerPointLabs.SyncLab.View
 {
@@ -20,206 +15,89 @@ namespace PowerPointLabs.SyncLab.View
     public partial class SyncFormatDialog : Window
     {
 
-        private bool checkboxChanging = false;
-        public SyncFormatDialog()
+        FormatTreeNode[] formats = null;
+
+        public SyncFormatDialog(Shape shape) : this(shape, SyncFormatConstants.FormatCategories)
         {
-            InitializeComponent();
-            KeyValuePair<string, string[]>[] items = new KeyValuePair<string, string[]>[]
-            {
-                new KeyValuePair<string, string[]>(
-                        "Text",
-                        new string[]
-                        {
-                                "Font",
-                                "Font Size",
-                                "Font Color",
-                                "Bold",
-                                "Italics",
-                                "Underline",
-                                "Shadow",
-                                "Strikethrow",
-                                "Character spacing",
-                                "Line spacing",
-                                "Alignment"
-                        }
-                    ),
-                new KeyValuePair<string, string[]>(
-                        "Fill",
-                        new string[]
-                        {
-                                "Fill"
-                        }
-                    ),
-                new KeyValuePair<string, string[]>(
-                        "Line",
-                        new string[]
-                        {
-                                "Start arrow",
-                                "End arrow",
-                                "Weight",
-                                "Style",
-                                "Fill"
-                        }
-                    ),
-                new KeyValuePair<string, string[]>(
-                        "Effect",
-                        new string[]
-                        {
-                                "Shadow",
-                                "Reflection",
-                                "Glow",
-                                "Soft Edge",
-                                "Bevel",
-                                "3D Rotation"
-                        }
-                    ),
-                new KeyValuePair<string, string[]>(
-                        "Position/Size",
-                        new string[]
-                        {
-                                "X",
-                                "Y",
-                                "Width",
-                                "Height"
-                        }
-                    ),
-            };
-            System.Drawing.Bitmap b = new System.Drawing.Bitmap(50, 50);
-            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(b);
-            g.FillRectangle(System.Drawing.Brushes.DarkBlue, 0, 0, b.Width, b.Height);
-            foreach (KeyValuePair<string, string[]> item in items)
-            {
-                TreeViewItem header = new TreeViewItem();
-                SyncFormatListItem i = new SyncFormatListItem();
-                List<SyncFormatListItem> children = new List<SyncFormatListItem>();
-                i.Text = item.Key;
-                i.Image = b;
-                RoutedEventHandler headerCheckChange = new System.Windows.RoutedEventHandler(
-                    (object sender, RoutedEventArgs e) =>
-                    {
-                        if (checkboxChanging)
-                        {
-                            return;
-                        }
-                        checkboxChanging = true;
-                        bool? checkStatus = ((SyncFormatListItem)((Grid)((CheckBox)sender).Parent).Parent).IsChecked;
-                        foreach (SyncFormatListItem obj in children)
-                        {
-                            obj.IsChecked = checkStatus;
-                        }
-                        checkboxChanging = false;
-                    }
-                );
-                i.checkBox.Checked += headerCheckChange;
-                i.checkBox.Unchecked += headerCheckChange;
-                i.checkBox.Indeterminate += headerCheckChange;
-                header.Header = i;
-                foreach (string format in item.Value)
-                {
-                    SyncFormatListItem j = new SyncFormatListItem();
-                    j.Text = format;
-                    j.Image = b;
-                    RoutedEventHandler checkChange = new System.Windows.RoutedEventHandler(
-                        (object sender, RoutedEventArgs e) =>
-                        {
-                            if (checkboxChanging)
-                            {
-                                return;
-                            }
-                            checkboxChanging = true;
-                            bool allChecked = true;
-                            bool allUnchecked = true;
-                            foreach (SyncFormatListItem subItem in children)
-                            {
-                                if (subItem.IsChecked.Value)
-                                {
-                                    allUnchecked = false;
-                                }
-                                else
-                                {
-                                    allChecked = false;
-                                }
-                            }
-                            if (allChecked)
-                            {
-                                i.IsChecked = true;
-                            }
-                            else if (allUnchecked)
-                            {
-                                i.IsChecked = false;
-                            }
-                            else
-                            {
-                                i.IsChecked = null;
-                            }
-                            checkboxChanging = false;
-                        }
-                    );
-                    j.checkBox.Checked += checkChange;
-                    j.checkBox.Unchecked += checkChange;
-                    j.checkBox.Indeterminate += checkChange;
-                    header.Items.Add(j);
-                    children.Add(j);
-                }
-                header.ExpandSubtree();
-                treeView.Items.Add(header);
-            }
         }
 
-        private void CheckBox_CheckedChanged(object sender, RoutedEventArgs e)
+        public SyncFormatDialog(Shape shape, FormatTreeNode[] formats)
         {
-            if (checkboxChanging)
+            InitializeComponent();
+            this.formats = formats;
+            foreach (FormatTreeNode format in formats)
             {
-                return;
-            }
-            checkboxChanging = true;
-            CheckBox checkBox = (CheckBox)sender;
-            SyncFormatListItem item = (SyncFormatListItem)((Grid)checkBox.Parent).Parent;
-            if (((TreeViewItem)item.Parent).Parent is TreeViewItem)
+                Object treeItem = DialogItemFromFormatTree(shape, format);
+                if (treeItem != null)
+                {
+                    treeView.Items.Add(treeItem);
+                }
+            }   
+        }
+
+        private Object DialogItemFromFormatTree(Shape shape, FormatTreeNode node)
+        {
+            if (node.Format != null)
             {
-                // Recalculate parent
-                bool allChecked = true;
-                bool allUnchecked = true;
-                foreach (object obj in ((TreeViewItem)item.Parent).Items)
+                SyncFormatDialogItem result = new SyncFormatDialogItem(node);
+                if (!node.Format.CanCopy(shape))
                 {
-                    SyncFormatListItem subItem = (SyncFormatListItem)obj;
-                    if (subItem.IsChecked.Value)
-                    {
-                        allUnchecked = false;
-                    }
-                    else
-                    {
-                        allChecked = false;
-                    }
+                    return null;
                 }
-                SyncFormatListItem parentItem = (SyncFormatListItem)((TreeViewItem)item.Parent).Header;
-                if (allChecked)
-                {
-                    parentItem.IsChecked = true;
-                }
-                else if (allUnchecked)
-                {
-                    parentItem.IsChecked = false;
-                }
-                else
-                {
-                    parentItem.IsChecked = null;
-                }
+                result.Text = node.Name;
+                result.IsChecked = node.IsChecked;
+                result.Image = node.Format.DisplayImage(shape);
+                return result;
             }
             else
             {
-                TreeViewItem parent = (TreeViewItem)item.Parent;
-                foreach (Object obj in parent.Items)
+                SyncFormatDialogItem header = new SyncFormatDialogItem(node);
+                header.Text = node.Name;
+                header.IsChecked = node.IsChecked;
+                TreeViewItem result = new TreeViewItem();
+                result.Header = header;
+                List<SyncFormatDialogItem> children = new List<SyncFormatDialogItem>();
+                foreach (FormatTreeNode childNode in node.ChildrenNodes)
                 {
-                    ((SyncFormatListItem)obj).IsChecked = item.IsChecked;
+                    Object treeItem = DialogItemFromFormatTree(shape, childNode);
+                    if (treeItem != null)
+                    {
+                        if (treeItem is SyncFormatDialogItem)
+                        {
+                            children.Add((SyncFormatDialogItem)treeItem);
+                        }
+                        else if (treeItem is TreeViewItem)
+                        {
+                            children.Add((SyncFormatDialogItem)((TreeViewItem)treeItem).Header);
+                        }
+                        result.Items.Add(treeItem);
+                    }
                 }
+                header.ItemChildren = children.ToArray<SyncFormatDialogItem>();
+                foreach (SyncFormatDialogItem child in children)
+                {
+                    child.ItemParent = header;
+                }
+                result.ExpandSubtree();
+                if (result.Items.Count == 0)
+                {
+                    return null;
+                }
+                return result;
             }
-            checkboxChanging = false;
         }
 
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
             this.DialogResult = true;
+        }
+
+        public FormatTreeNode[] Formats
+        {
+            get
+            {
+                return formats;
+            }
         }
     }
 }
