594bcd72bb57226a3aa3aaa780512b528edf5127
initialshl <initialshl@outlook.com>
2017-02-27 23:36:39 +0800

    Speed up CropOutPadding by loading into memory

--- PowerPointLabs/PowerPointLabs/CropLab/CropLabErrorHandler.cs
+++ PowerPointLabs/PowerPointLabs/CropLab/CropLabErrorHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace PowerPointLabs.CropLab
+﻿using System;
+
+namespace PowerPointLabs.CropLab
 {
     internal class CropLabErrorHandler
     {
@@ -55,6 +57,15 @@
                     ShowErrorMessage(errorCode);
                     break;
             }
+        }
+
+        public void ProcessException(Exception e, string message)
+        {
+            if (View == null) // Nothing to display on
+            {
+                return;
+            }
+            View.ShowErrorMessageBox(message, e);
         }
 
         /// <summary>
