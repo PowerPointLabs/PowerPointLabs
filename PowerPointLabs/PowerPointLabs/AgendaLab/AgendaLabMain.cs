6c3928bb7c620dfdff11e0779aace747e5abdb3d
Baron <a0122545@u.nus.edu>
2017-03-03 18:00:31 +0800

    Syncing Sidebar agenda should cause all highlighted agenda titles on each page to be aligned correctly

--- PowerPointLabs/PowerPointLabs/AgendaLab/AgendaLabMain.cs
+++ PowerPointLabs/PowerPointLabs/AgendaLab/AgendaLabMain.cs
@@ -1016,8 +1016,10 @@ namespace PowerPointLabs.AgendaLab
                                                                                             shape.Section.Index == section.Index))
                                                 .FirstOrDefault();
             var currentSectionText = currentSectionTextBox.TextFrame2.TextRange;
+            var originalAlignment = currentSectionTextBox.TextFrame2.TextRange.ParagraphFormat.Alignment;
 
             Graphics.SyncTextRange(beamFormats.Highlighted, currentSectionText, pickupTextContent: false);
+            currentSectionTextBox.TextFrame2.TextRange.ParagraphFormat.Alignment = originalAlignment;
         }
         #endregion
 
