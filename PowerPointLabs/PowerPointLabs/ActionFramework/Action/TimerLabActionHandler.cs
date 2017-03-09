f84e070fdb300321c0eaaaccfe6bfd4b45195cfd
Ang Shi Ya <shiya_95@hotmail.com>
2017-03-09 13:36:46 +0800

    Add Blocks Timer Prototype (#1176)
    * add blocks timer prototype
    
    * Add icon for Timer Lab
    
    * Update PowerPointLabs.csproj
    
    Fix merge error in PowerPointLabs.csproj

---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Action/TimerLabActionHandler.cs
@@ -0,0 +1,27 @@
+﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Extension;
+using PowerPointLabs.ActionFramework.Common.Interface;
+using PowerPointLabs.TimerLab;
+
+namespace PowerPointLabs.ActionFramework.Action
+{
+    [ExportActionRibbonId("TimerLabButton")]
+    class TimerLabActionHandler : ActionHandler
+    {
+        protected override void ExecuteAction(string ribbonId)
+        {
+            this.RegisterTaskPane(typeof(TimerPane), TextCollection.TimerLabTaskPaneTitle);
+            var timerPane = this.GetTaskPane(typeof(TimerPane));
+            // if currently the pane is hidden, show the pane
+            if (!timerPane.Visible)
+            {
+                // fire the pane visble change event
+                timerPane.Visible = true;
+            }
+            else
+            {
+                timerPane.Visible = false;
+            }
+        }
+    }
+}
