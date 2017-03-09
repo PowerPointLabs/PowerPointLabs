f8ab62856a3769a5dad8f93f70c385ee64ec1600
Ian Teo <ian_teo@msn.com>
2017-03-09 12:12:55 +0800

    Sync labs Core functionality (#1175)
    * Add Sync Labs Infrastructure
    
    * Add Sync Labs basic functionality

---
+++ PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/LineFillFormat.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace PowerPointLabs.SyncLab.ObjectFormats
+{
+    class LineFillFormat
+    {
+        public static bool CanCopy(Shape formatShape)
+        {
+            return true;
+        }
+
+        public static void SyncFormat(Shape formatShape, Shape newShape)
+        {
+            //TODO not working
+            newShape.Line.ForeColor = newShape.Line.ForeColor;
+            newShape.Line.BackColor = formatShape.Line.BackColor;
+        }
+
+        public static Bitmap DisplayImage(Shape formatShape)
+        {
+            Bitmap b = new Bitmap(200, 200);
+            Graphics g = Graphics.FromImage(b);
+            g.FillRectangle(Brushes.DarkBlue, 0, 0, 200, 200);
+            return b;
+        }
+    }
+}
---
+++ PowerPointLabs/PowerPointLabs/SyncLab/View/SyncPane.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+
+namespace PowerPointLabs.SyncLab.View
+{
+    public partial class SyncPane : UserControl
+    {
+
+        private bool _firstTimeLoading = true;
+
+        public SyncPane()
+        {
+            InitializeComponent();
+        }
+
+        #region API
+        public void PaneReload(bool forceReload = false)
+        {
+            if (!_firstTimeLoading && !forceReload)
+            {
+                return;
+            }
+
+            _firstTimeLoading = false;
+        }
+        #endregion
+
+    }
+}
