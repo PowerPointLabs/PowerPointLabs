b08d6d04833abcb3e64ffe509469c599dfcd610e
Ian <teozhiwen@gmail.com>
2017-03-21 19:31:40 +0800

    Add support for sync background fill and sync pattern fill

--- PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/FillFormat.cs
+++ PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/FillFormat.cs
@@ -41,9 +41,29 @@ namespace PowerPointLabs.SyncLab.ObjectFormats
         {
             try
             {
-                newShape.Fill.ForeColor = formatShape.Fill.ForeColor;
-                newShape.Fill.BackColor = formatShape.Fill.BackColor;
-                newShape.Fill.Solid();
+                if (formatShape.Fill.Type == Microsoft.Office.Core.MsoFillType.msoFillPatterned)
+                {
+                    newShape.Fill.Patterned(formatShape.Fill.Pattern);
+                    newShape.Fill.ForeColor = formatShape.Fill.ForeColor;
+                    newShape.Fill.BackColor = formatShape.Fill.BackColor;
+                }
+                else if (formatShape.Fill.Type == Microsoft.Office.Core.MsoFillType.msoFillBackground)
+                {
+                    newShape.Fill.Background();
+                }
+                else if (formatShape.Fill.Type == Microsoft.Office.Core.MsoFillType.msoFillSolid)
+                {
+                    newShape.Fill.Solid();
+                    newShape.Fill.ForeColor = formatShape.Fill.ForeColor;
+                }
+                else if (formatShape.Fill.Type == Microsoft.Office.Core.MsoFillType.msoFillTextured)
+                {
+                    newShape.Fill.PresetTextured(formatShape.Fill.PresetTexture);
+                }
+                else
+                {
+                    //Unable to set no fill
+                }
             }
             catch (Exception)
             {
