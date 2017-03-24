5687f206f47fc668c0f5bdc8ad756085919af93c
initialshl <initialshl@outlook.com>
2017-03-24 10:38:55 +0800

    Implement Crop Lab Settings for CropToAspectRatio

---
+++ PowerPointLabs/PowerPointLabs/CropLab/CropLabSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace PowerPointLabs.CropLab
+{
+    public enum AnchorPosition
+    {
+        TopLeft,
+        Top,
+        TopRight,
+        MiddleLeft,
+        Middle,
+        MiddleRight,
+        BottomLeft,
+        Bottom,
+        BottomRight
+    }
+
+    internal static class CropLabSettings
+    {
+        private static AnchorPosition anchorPosition = AnchorPosition.Middle;
+
+        public static AnchorPosition AnchorPosition
+        {
+            get { return anchorPosition; }
+            set { anchorPosition = value; }
+        }
+    }
+}
---
+++ PowerPointLabs/PowerPointLabs/CropLab/CropLabSettingsDialog.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace PowerPointLabs.CropLab
+{
+    /// <summary>
+    /// Interaction logic for CropLabSettingsDialog.xaml
+    /// </summary>
+    public partial class CropLabSettingsDialog
+    {
+        private AnchorPosition selectedAnchor;
+
+        public CropLabSettingsDialog()
+        {
+            selectedAnchor = CropLabSettings.AnchorPosition;
+            InitializeComponent();
+        }
+
+        public AnchorPosition SelectedAnchor
+        {
+            get { return selectedAnchor; }
+            set { selectedAnchor = value; }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            CropLabSettings.AnchorPosition = selectedAnchor;
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
