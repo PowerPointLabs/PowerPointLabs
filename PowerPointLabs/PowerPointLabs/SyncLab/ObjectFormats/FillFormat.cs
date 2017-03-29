35729ffae501a8a7e88e260c0234cfeb22a466eb
Ian Teo <ian_teo@msn.com>
2017-03-29 23:01:52 +0800

    Fix sync color for fill and line to sync the exact color RGB (#1257)

--- PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/FillFormat.cs
+++ PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/FillFormat.cs
@@ -47,8 +47,8 @@ namespace PowerPointLabs.SyncLab.ObjectFormats
                 if (formatShape.Fill.Type == Microsoft.Office.Core.MsoFillType.msoFillPatterned)
                 {
                     newShape.Fill.Patterned(formatShape.Fill.Pattern);
-                    newShape.Fill.ForeColor = formatShape.Fill.ForeColor;
-                    newShape.Fill.BackColor = formatShape.Fill.BackColor;
+                    newShape.Fill.ForeColor.RGB = formatShape.Fill.ForeColor.RGB;
+                    newShape.Fill.BackColor.RGB = formatShape.Fill.BackColor.RGB;
                 }
                 else if (formatShape.Fill.Type == Microsoft.Office.Core.MsoFillType.msoFillBackground)
                 {
@@ -57,7 +57,7 @@ namespace PowerPointLabs.SyncLab.ObjectFormats
                 else if (formatShape.Fill.Type == Microsoft.Office.Core.MsoFillType.msoFillSolid)
                 {
                     newShape.Fill.Solid();
-                    newShape.Fill.ForeColor = formatShape.Fill.ForeColor;
+                    newShape.Fill.ForeColor.RGB = formatShape.Fill.ForeColor.RGB;
                 }
                 else if (formatShape.Fill.Type == Microsoft.Office.Core.MsoFillType.msoFillGradient)
                 {
