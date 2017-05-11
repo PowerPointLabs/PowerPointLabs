b98d1a0e5406e930f5d6609a40f62d6fcd0c4527
Ong Heng Le <initialshl@outlook.com>
2017-05-11 15:20:58 +0800

    Make all Paste Lab features preserve clipboard items (#1318)

---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Action/PasteLab/PasteLabActionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+using Microsoft.Office.Interop.PowerPoint;
+
+using PowerPointLabs.ActionFramework.Common.Extension;
+using PowerPointLabs.ActionFramework.Common.Interface;
+
+namespace PowerPointLabs.ActionFramework.Action.PasteLab
+{
+    abstract class PasteLabActionHandler : ActionHandler
+    {
+        // Sealed method: Subclasses should override ExecutePasteAction instead
+        protected sealed override void ExecuteAction(string ribbonId)
+        {
+            // Store and restore clipboard data:
+            // Reason for not using Clipboard.SetDataObject(): it does not preserve position
+            var tempSlide = this.GetCurrentPresentation().AddSlide();
+            ShapeRange clipboardItems = tempSlide.Shapes.Paste();
+
+            this.StartNewUndoEntry();
+            ExecutePasteAction(ribbonId);
+
+            clipboardItems.Copy();
+            tempSlide.Delete();
+        }
+
+        protected abstract void ExecutePasteAction(string ribbonId);
+    }
+}
