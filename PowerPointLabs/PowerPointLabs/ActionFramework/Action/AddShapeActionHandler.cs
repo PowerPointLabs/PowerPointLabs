dce24f1cd3e85a36a323445055f1d21600edafd3
Ong Heng Le <initialshl@outlook.com>
2017-05-11 21:52:53 +0800

    Preserve shape name when adding to shapes lab (#1305)

--- PowerPointLabs/PowerPointLabs/ActionFramework/Action/AddShapeActionHandler.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Action/AddShapeActionHandler.cs
@@ -22,8 +22,7 @@ namespace PowerPointLabs.ActionFramework.Action
             }
 
             // add shape into shape gallery first to reduce flicker
-            var shapeName = addIn.ShapePresentation.AddShape(selection,
-                                                                         TextCollection.CustomShapeDefaultShapeName);
+            var shapeName = addIn.ShapePresentation.AddShape(selection, selection.ShapeRange[1].Name);
 
             // add the selection into pane and save it as .png locally
             var shapeFullName = Path.Combine(customShape.CurrentShapeFolderPath, shapeName + ".png");
