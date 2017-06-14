7543c48dd5809bfa58b24dcaa2754007601bd558
IanTeo <ian_teo@msn.com>
2017-06-14 15:53:57 +0800

    Improve EditName to be able to change name of item in a group

--- PowerPointLabs/PowerPointLabs/Views/Form1.cs
+++ PowerPointLabs/PowerPointLabs/Views/Form1.cs
@@ -1,34 +1,35 @@
 ﻿using System;
 using System.Windows.Forms;
 
+using Microsoft.Office.Interop.PowerPoint;
+
 namespace PowerPointLabs.Views
 {
     public partial class Form1 : Form
     {
-        public string newName;
+        public Shape shape;
         private Ribbon1 ribbon;
         public Form1()
         {
             InitializeComponent();
         }
 
-        public Form1(Ribbon1 parentRibbon, String oldName)
+        public Form1(Ribbon1 parentRibbon, Shape selectedShape)
             : this()
         {
             ribbon = parentRibbon;
-            newName = oldName;
+            shape = selectedShape;
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            this.newName = this.textBox1.Text;
-            ribbon.ShapeNameEdited(this.newName);
+            this.shape.Name = this.textBox1.Text;
             this.Close();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.textBox1.Text = newName;
+            this.textBox1.Text = shape.Name;
         }
 
         private void Button2_Click(object sender, EventArgs e)
