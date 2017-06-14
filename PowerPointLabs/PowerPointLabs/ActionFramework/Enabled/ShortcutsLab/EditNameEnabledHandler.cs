28575dea5370b4ae5fbe991297fa04f96dde667e
IanTeo <ian_teo@msn.com>
2017-06-14 13:36:44 +0800

    Disable context menu for edit name when multiple shapes selected

---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Enabled/ShortcutsLab/EditNameEnabledHandler.cs
@@ -0,0 +1,20 @@
+﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Interface;
+using PowerPointLabs.Utils;
+
+namespace PowerPointLabs.ActionFramework.Enabled.PasteLab
+{
+    [ExportEnabledRibbonId(
+        "EditNameMenuShape", "EditNameMenuLine", "EditNameMenuFreeform",
+        "EditNameMenuPicture", "EditNameMenuGroup", "EditNameMenuInk",
+        "EditNameMenuVideo", "EditNameMenuTextEdit", "EditNameMenuChart",
+        "EditNameMenuTable", "EditNameMenuTableWhole", "EditNameMenuSmartArtBackground",
+        "EditNameMenuSmartArtEditSmartArt", "EditNameMenuSmartArtEditText")]
+    class EditNameEnabledHandler : EnabledHandler
+    {
+        protected override bool GetEnabled(string ribbonId)
+        {
+            return IsSelectionSingleShape();
+        }
+    }
+}
