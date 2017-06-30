eafd15610ae0267d5e4488185c488d51198edc14
IanTeo <ian_teo@msn.com>
2017-06-30 13:04:06 +0800

    Fix using errors

--- PowerPointLabs/PowerPointLabs/ActionFramework/Common/Factory/SupertipHandlerFactory.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Common/Factory/SupertipHandlerFactory.cs
@@ -1,5 +1,5 @@
-﻿using PowerPointLabs.ActionFramework.Common.Interface;
-using PowerPointLabs.ActionFramework.Supertip;
+﻿using PowerPointLabs.ActionFramework.Common.Handlers;
+using PowerPointLabs.ActionFramework.Common.Interface;
 
 namespace PowerPointLabs.ActionFramework.Common.Factory
 {
--- PowerPointLabs/PowerPointLabs/ActionFramework/RibbonGroups/MoreLabsGroupLabelHandler.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/RibbonGroups/MoreLabsGroupLabelHandler.cs
@@ -4,7 +4,7 @@ using PowerPointLabs.ActionFramework.Common.Interface;
 namespace PowerPointLabs.ActionFramework.RibbonGroups
 {
     [ExportLabelRibbonId(TextCollection.MoreLabsGroupId)]
-    class FormattingGroupLabelHandler : LabelHandler
+    class MoreLabsGroupLabelHandler : LabelHandler
     {
         protected override string GetLabel(string ribbonId)
         {
