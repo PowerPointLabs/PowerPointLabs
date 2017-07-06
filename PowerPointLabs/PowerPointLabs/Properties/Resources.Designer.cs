ab34aa69088f9d01c1a02948421e0cb758257367
Ong Heng Le <initialshl@outlook.com>
2017-07-06 16:34:24 +0800

    Remove white outlines from all lab icons (#1502)

--- PowerPointLabs/PowerPointLabs/Properties/Resources.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Resources.Designer.cs
@@ -1203,16 +1203,6 @@ namespace PowerPointLabs.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap PictureSlidesLab1 {
-            get {
-                object obj = ResourceManager.GetObject("PictureSlidesLab1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         public static System.Drawing.Bitmap Play {
             get {
                 object obj = ResourceManager.GetObject("Play", resourceCulture);
