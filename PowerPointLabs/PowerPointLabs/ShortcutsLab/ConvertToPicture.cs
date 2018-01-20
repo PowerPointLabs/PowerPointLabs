0db49e3b1c2e897c27155666602493c2fa2d5f83
junkiattan <cyan4936@gmail.com>
2018-01-20 16:30:53 +0800

    changed variable types back to original

--- PowerPointLabs/PowerPointLabs/ShortcutsLab/ConvertToPicture.cs
+++ PowerPointLabs/PowerPointLabs/ShortcutsLab/ConvertToPicture.cs
@@ -20,7 +20,7 @@ namespace PowerPointLabs.ShortcutsLab
         {
             if (ShapeUtil.IsSelectionShapeOrText(selection))
             {
-                PowerPoint.Shape shape = GetShapeFromSelection(selection);
+                var shape = GetShapeFromSelection(selection);
                 zOrder = shape.ZOrderPosition;
                 shape = CutPasteShape(shape);
                 ConvertToPictureForShape(shape);
@@ -68,7 +68,7 @@ namespace PowerPointLabs.ShortcutsLab
             float width = shape.Width;
             float height = shape.Height;
             shape.Delete();
-            PowerPoint.Shape pic = PowerPointCurrentPresentationInfo.CurrentSlide.Shapes.PasteSpecial(PowerPoint.PpPasteDataType.ppPastePNG)[1];
+            var pic = PowerPointCurrentPresentationInfo.CurrentSlide.Shapes.PasteSpecial(PowerPoint.PpPasteDataType.ppPastePNG)[1];
             pic.Left = x + (width - pic.Width) / 2;
             pic.Top = y + (height - pic.Height) / 2;
             pic.Rotation = rotation;
