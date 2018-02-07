00af53be7f3504b0dddeba93a56c261ac119b491
junkiattan <cyan4936@gmail.com>
2018-02-07 15:27:38 +0800

    Initial contribution to Save Lab Action Framework and Save Lab Text in Text Collection

---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/SaveLab/SaveLabMenu/SaveLabMenuImageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Interface;
+using PowerPointLabs.TextCollection;
+
+namespace PowerPointLabs.ActionFramework.CropLab
+{
+    [ExportImageRibbonId(SaveLabText.RibbonMenuId)]
+    class SaveLabMenuImageHandler : ImageHandler
+    {
+        protected override Bitmap GetImage(string ribbonId)
+        {
+            // Need a new icon for Save Lab
+            return new Bitmap(Properties.Resources.CropLab);
+        }
+    }
+}
---
+++ PowerPointLabs/PowerPointLabs/TextCollection/SaveLabText.cs
@@ -0,0 +1,23 @@
+﻿namespace PowerPointLabs.TextCollection
+{
+    internal static class SaveLabText
+    {
+        #region Action Framework Tags
+        public const string RibbonMenuId = "SaveLabMenu";
+        public const string SaveTag = "SavePresentations";
+        #endregion
+
+        #region GUI Text
+        public const string RibbonMenuLabel = "Save";
+        public const string SaveButtonLabel = "Save Presentations";
+
+        public const string RibbonMenuSupertip = "Use Save Lab to save presentations in a separate PowerPoint file.";
+        public const string SaveButtonSupertip =
+            "Save presentations to a desired file and location.\n\n" +
+            "To perform this action, select the slides to export, then click this button.";
+
+        public const string ErrorSavingPresentations = "Presentations could not be saved.";
+        
+        #endregion
+    }
+}
