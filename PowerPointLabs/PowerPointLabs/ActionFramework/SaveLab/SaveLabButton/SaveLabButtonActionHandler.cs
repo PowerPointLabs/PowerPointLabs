808f92425688d16f28c6aa7d5b18f46ca6b42fb8
junkiattan <cyan4936@gmail.com>
2018-02-20 17:32:08 +0800

    Tried implementing save functionality

--- PowerPointLabs/PowerPointLabs/ActionFramework/SaveLab/SaveLabButton/SaveLabButtonActionHandler.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/SaveLab/SaveLabButton/SaveLabButtonActionHandler.cs
@@ -1,4 +1,7 @@
-﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+﻿using Microsoft.Office.Interop.PowerPoint;
+
+using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Extension;
 using PowerPointLabs.SaveLab;
 using PowerPointLabs.TextCollection;
 
@@ -10,6 +13,21 @@ namespace PowerPointLabs.ActionFramework.SaveLab
         protected override void ExecuteAction(string ribbonId)
         {
             // Save action here
+
+            // Get selection of the slides
+            Selection currentSelection = this.GetCurrentSelection();
+             //check the type of selection and ensure that it is a slide and that the number of slides selected is >= 1
+            if (currentSelection.Type == PpSelectionType.ppSelectionSlides && currentSelection.SlideRange.Count >= 1)
+            {
+                // Perform the actual save action
+                SaveLabMain.SaveFile(currentSelection.SlideRange);
+            }
+            else
+            {
+                //if no slides return error message or do nothing
+                return;
+            }
+            // Save selection as a separate ppt file
         }
     }
 }
---
+++ PowerPointLabs/PowerPointLabs/SaveLab/SaveLabMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace PowerPointLabs.SaveLab
+{
+    class SaveLabMain
+    {
+        public static void SaveFile(SlideRange selectedSlides)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = SaveLabSettings.SaveFolderPath;
+            saveFileDialog.Filter = "PowerPoint Presentations|*.ppt";
+            saveFileDialog.Title = "Save Selected Slides";
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Creates a new presentation and removes the first slide
+                Presentation newPresentation = new Presentation();
+                newPresentation.Slides.FindBySlideID(1).Delete();
+                // Copies over the selected slides
+                for (int i = 0; i < selectedSlides.Count; i++)
+                {
+                    newPresentation.Slides.AddSlide(i + 1, selectedSlides[1].CustomLayout);
+                }
+                //Save the new presentation as a new name
+                newPresentation.SaveAs(saveFileDialog.FileName);
+                System.Diagnostics.Process.Start(saveFileDialog.FileName + ".pptx");
+            }
+
+        }
+    }
+}
