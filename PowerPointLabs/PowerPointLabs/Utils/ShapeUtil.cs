4475e0eb9d37f94bc23b79b26f40133dbfbc8afd
Alex Fong <11351531+alexfjw@users.noreply.github.com>
2018-02-24 21:11:54 +0800

    #1544-Copy-Operation-Made-The-Text-Disappear (#1617)
    * Guess shape's text color instead of using MS API
    
    Microsoft's API is buggy and returns black when shape's text has more
    than 1 color.
    
    Fix: Clear the TextRange & guess the color instead.
    
    * docs: added new findings in comments
    
    * docs: shifted comments

--- PowerPointLabs/PowerPointLabs/Utils/ShapeUtil.cs
+++ PowerPointLabs/PowerPointLabs/Utils/ShapeUtil.cs
@@ -984,6 +984,28 @@ namespace PowerPointLabs.Utils
             return shape == null ? null : shape.TextFrame.TextRange;
         }
 
+        /// <summary>
+        /// Useful when shape has text with more than one color
+        /// Getting color in that case without a workabout will return black
+        /// Assumes that shape has a TextRange
+        /// </summary>
+        /// <param name="shape">RGB color, seems to be the color of the last character</param>
+        /// <returns></returns>
+        public static int GuessTextColor(Shape shape)
+        {
+            // clear the text, to clear presence of more than 1 color
+            // TextRange.Font.Color.RGB then returns the same color of as new text that is added to the shape
+            // 
+            // Unsuccessful workabouts:
+            // Taking a sub TextRange of the shape
+            // Trimming text to the first character
+            Shape duplicate = shape.Duplicate()[1];
+            duplicate.TextFrame.TextRange.Text = "";
+            int color = duplicate.TextFrame.TextRange.Font.Color.RGB;
+            duplicate.Delete();
+            return color;
+        }
+
         #endregion
         
         /// <summary>
