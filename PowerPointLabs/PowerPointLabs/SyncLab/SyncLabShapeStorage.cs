2a26c0598a99574a4c7c3d37e6adad7d0e8f7902
Lee Yan Hwa <leeyh20@users.noreply.github.com>
2018-03-23 22:43:20 +0800

    #1608 Fixes no currently active document window (#1613)
    * Tries to activate window when getting selection but uses null
    
    * Combine ifs
    
    * Refactor Open In Background method to use Open method with focus = false
    
    * Add null checks and remove activate window
    
    * Minor fix to brackets
    
    * Workaround to make PPT visible as described by Microsoft Support
    
    * Grey out menu if no presentation is open
    
    * More null checks
    
    * Refresh menu buttons on presentation open
    
    * Fix error that appears when preview of template slides appears
    
    * Use static text for refreshing menu buttons
    
    * Use selection variable
    
    * Remove lines Ribbon.xml
    
    * Minor edits
    
    * Refresh ribbon when new presentation is opened
    
    * Remove duplicate refresh ribbon control add spotlight

--- PowerPointLabs/PowerPointLabs/SyncLab/SyncLabShapeStorage.cs
+++ PowerPointLabs/PowerPointLabs/SyncLab/SyncLabShapeStorage.cs
@@ -35,7 +35,7 @@ namespace PowerPointLabs.SyncLab
         {
             Path = System.IO.Path.GetTempPath();
             Name = SyncLabText.StorageFileName;
-            OpenInBackground();
+            Open(withWindow: false, focus: false);
             ClearShapes();
         }
 
@@ -118,7 +118,7 @@ namespace PowerPointLabs.SyncLab
         {
             Save();
             Close();
-            OpenInBackground();
+            Open(withWindow: false, focus: false);
         }
 
         public void ClearShapes()
