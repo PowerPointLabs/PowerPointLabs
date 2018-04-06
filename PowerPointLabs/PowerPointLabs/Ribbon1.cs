5aae97be2f4034c1828eb6178b18ad9d262ca557
junkiattan <cyan4936@gmail.com>
2018-04-06 23:06:20 +0800

    Changed method name to better reflect function

--- PowerPointLabs/PowerPointLabs/Ribbon1.cs
+++ PowerPointLabs/PowerPointLabs/Ribbon1.cs
@@ -139,7 +139,7 @@ namespace PowerPointLabs
         //Create callback methods here. For more information about adding callback methods, select the Ribbon XML item in Solution Explorer and then press F1
 
         // Set the visibility of the Drawing Tools Format Tab
-        public bool SetVisibleFormatTab(Office.IRibbonControl control)
+        public bool ToggleVisibleFormatTab(Office.IRibbonControl control)
         {
             return !disableFormatTab;
         }
