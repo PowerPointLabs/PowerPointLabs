890950d06673f1101fc42499a2a9813f9f5dcbd9
Alex Fong <alexfongg@gmail.com>
2018-04-11 15:15:10 +0800

    Change SoftEdge preview to number

--- PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/SoftEdgeEffectFormat.cs
+++ PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/SoftEdgeEffectFormat.cs
@@ -28,22 +28,10 @@ namespace PowerPointLabs.SyncLab.ObjectFormats
 
         public override Bitmap DisplayImage(Shape formatShape)
         {
-            // bump up soft edge to make effect in preview more visible
-            // work on a duplicate to avoid complex control flow to revert SoftEdge.Type
-            // see comments in Sync(..) for more details
-            Shape duplicate = formatShape.Duplicate()[1];
-            duplicate.SoftEdge.Type = MsoSoftEdgeType.msoSoftEdgeType6;
-            float minEdge = Math.Min(formatShape.Height, formatShape.Width);
-            float threshold = (float) (minEdge * 0.2);
-            if (duplicate.SoftEdge.Radius < threshold)
-            {
-                formatShape.SoftEdge.Radius = threshold;
-            }
-            
-            Bitmap image = GraphicsUtil.ShapeToBitmap(duplicate);
-            duplicate.Delete();
-            
-            return image;
+            return SyncFormatUtil.GetTextDisplay(
+                Math.Round(formatShape.SoftEdge.Radius, 1).ToString(),
+                SyncFormatConstants.DisplayImageFont,
+                SyncFormatConstants.DisplayImageSize);
         }
 
         private static bool Sync(Shape formatShape, Shape newShape)
