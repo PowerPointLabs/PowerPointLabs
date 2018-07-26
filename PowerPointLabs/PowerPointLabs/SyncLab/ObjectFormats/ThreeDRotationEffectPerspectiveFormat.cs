646ac39994ea446cfad2ef8cd0d00fb599ec75d0
tankztz <tankztz@gmail.com>
2018-07-26 14:09:49 +0800

    Change tabs to spaces

--- PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/ThreeDRotationEffectPerspectiveFormat.cs
+++ PowerPointLabs/PowerPointLabs/SyncLab/ObjectFormats/ThreeDRotationEffectPerspectiveFormat.cs
@@ -52,8 +52,8 @@ namespace PowerPointLabs.SyncLab.ObjectFormats
             
             try
             {
-                // set perspective only if it is different,	
-                // setting the same perspective applies an unknown change to the lighting of the shape	
+                // set perspective only if it is different,
+                // setting the same perspective applies an unknown change to the lighting of the shape
                 // this change is visible to the eye, but we cannot undo it
                 if (dest.Perspective != source.Perspective)
                 {
