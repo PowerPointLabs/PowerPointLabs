b08cb858d6b248911bf2a65832f4069ebc70fe9f
leeyh20 <leeyh20@hotmail.com>
2019-01-02 00:49:20 +0800

    Change version number for fix

--- PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace PowerPointLabs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("4.3.0.1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("4.3.0.2")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
@@ -70,7 +70,7 @@ namespace PowerPointLabs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("18 April 2018")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2 October 2018")]
         public string ReleaseDate {
             get {
                 return ((string)(this["ReleaseDate"]));
