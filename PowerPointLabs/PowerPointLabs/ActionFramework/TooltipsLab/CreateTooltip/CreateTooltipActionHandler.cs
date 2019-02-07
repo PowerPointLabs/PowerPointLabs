c7369b27c1c249f778699cea67aec1ab717e8c3f
blewjy <blewjy@gmail.com>
2019-02-07 18:08:54 +0800

    Init new lab with placeholder menu: TooltipsLab

---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/TooltipsLab/CreateTooltip/CreateTooltipActionHandler.cs
@@ -0,0 +1,19 @@
+﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Extension;
+using PowerPointLabs.ActionFramework.Common.Interface;
+using PowerPointLabs.AnimationLab;
+using PowerPointLabs.TextCollection;
+
+namespace PowerPointLabs.ActionFramework.TooltipsLab
+{
+    [ExportActionRibbonId(TooltipsLabText.CreateTooltipTag)]
+    class CreateTooltipActionHandler : ActionHandler
+    {
+        protected override void ExecuteAction(string ribbonId)
+        {
+            this.StartNewUndoEntry();
+
+            AutoAnimate.AddAutoAnimation();
+        }
+    }
+}
