333bfb6ff42f1868b742dc38dba87e2ff3f4380b
blewjy <blewjy@gmail.com>
2019-02-07 18:08:54 +0800

    Init new lab with placeholder menu: TooltipsLab

---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/TooltipsLab/CreateTooltip/CreateTooltipLabelHandler.cs
@@ -0,0 +1,15 @@
+﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Interface;
+using PowerPointLabs.TextCollection;
+
+namespace PowerPointLabs.ActionFramework.TooltipsLab
+{
+    [ExportLabelRibbonId(TooltipsLabText.CreateTooltipTag)]
+    class CreateTooltipLabelHandler : LabelHandler
+    {
+        protected override string GetLabel(string ribbonId)
+        {
+            return TooltipsLabText.CreateTooltipButtonLabel;
+        }
+    }
+}
