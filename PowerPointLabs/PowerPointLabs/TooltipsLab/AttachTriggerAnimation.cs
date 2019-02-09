4ecde7d36537baa8960063fc9fc5e8f653de0096
ChesterSng <chester_nutty@hotmail.com>
2019-02-09 17:06:42 +0800

    Add AttachTriggerAnimation.cs

---
+++ PowerPointLabs/PowerPointLabs/TooltipsLab/AttachTriggerAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Office.Interop.PowerPoint;
+using PowerPointLabs.Models;
+using PowerPointLabs.Utils;
+
+namespace PowerPointLabs.TooltipsLab
+{
+    internal static class AttachTriggerAnimation
+    {
+        public static void AddTriggerAnimation(PowerPointSlide currentSlide, Selection selection)
+        {
+            try
+            {
+                ShapeRange selectedShapes = selection.ShapeRange;
+
+                AddTriggerAnimation(currentSlide, selectedShapes);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        private static void AddTriggerAnimation(PowerPointSlide currentSlide, ShapeRange shapes)
+        {
+            MsoAnimTriggerType trigger = MsoAnimTriggerType.msoAnimTriggerOnPageClick;
+
+            foreach (Shape animationShape in shapes)
+            {
+                Effect effectRotate = currentSlide.TimeLine.MainSequence.AddEffect(animationShape, MsoAnimEffect.msoAnimEffectSpin, MsoAnimateByLevel.msoAnimateLevelNone, trigger);
+                AnimationBehavior rotate = effectRotate.Behaviors[1];
+                effectRotate.Timing.Duration = 1.0F;
+                effectRotate.EffectParameters.Amount = LegacyShapeUtil.GetMinimumRotation(0.0F, 180.0F);
+            }
+
+        }
+
+    }
+}
