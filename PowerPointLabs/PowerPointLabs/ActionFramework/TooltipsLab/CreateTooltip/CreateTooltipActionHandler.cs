22c92c84affbac09495f85e84deab24809fe4356
ChesterSng <chester_nutty@hotmail.com>
2019-02-09 17:06:15 +0800

    Create skeleton for adding trigger animation to shape

--- PowerPointLabs/PowerPointLabs/ActionFramework/TooltipsLab/CreateTooltip/CreateTooltipActionHandler.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/TooltipsLab/CreateTooltip/CreateTooltipActionHandler.cs
@@ -1,8 +1,12 @@
-﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+﻿using Microsoft.Office.Interop.PowerPoint;
+using PowerPointLabs.ActionFramework.Common.Attribute;
 using PowerPointLabs.ActionFramework.Common.Extension;
 using PowerPointLabs.ActionFramework.Common.Interface;
 using PowerPointLabs.AnimationLab;
+using PowerPointLabs.Models;
 using PowerPointLabs.TextCollection;
+using PowerPointLabs.TooltipsLab;
+using PowerPointLabs.Utils;
 
 namespace PowerPointLabs.ActionFramework.TooltipsLab
 {
@@ -11,9 +15,15 @@ namespace PowerPointLabs.ActionFramework.TooltipsLab
     {
         protected override void ExecuteAction(string ribbonId)
         {
-            this.StartNewUndoEntry();
+            Selection selection = this.GetCurrentSelection();
+            PowerPointSlide currentSlide = this.GetCurrentSlide();
 
-            AutoAnimate.AddAutoAnimation();
+            if (!ShapeUtil.IsSelectionShape(selection))
+            {
+                return;
+            }
+
+            AttachTriggerAnimation.AddTriggerAnimation(currentSlide, selection);
         }
     }
 }
