27e98ec8c56f89767bf09c0810b7054b98099aa7
blewjy <blewjy@gmail.com>
2019-02-09 19:05:53 +0800

    Paint and bind brightness and saturation rectangles

---
+++ PowerPointLabs/PowerPointLabs/Converters/ColorPane/SelectedColorToMaximumBrightnessHex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Data;
+
+namespace PowerPointLabs.Converters.ColorPane
+{
+    [ValueConversion(typeof(HSLColor), typeof(string))]
+    class SelectedColorToMaximumBrightnessHex : IValueConverter
+    {
+
+        public static SelectedColorToMaximumBrightnessHex Instance = new SelectedColorToMaximumBrightnessHex();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                Color color = (HSLColor)value;
+                return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+            }
+            HSLColor selectedColor = (HSLColor)value;
+            HSLColor maxBrightnessHslColor = new HSLColor();
+            maxBrightnessHslColor.Hue = selectedColor.Hue;
+            maxBrightnessHslColor.Saturation = selectedColor.Saturation;
+            maxBrightnessHslColor.Luminosity = 240;
+            Color maxBrightnessColor = maxBrightnessHslColor;
+            return "#" + maxBrightnessColor.R.ToString("X2") + maxBrightnessColor.G.ToString("X2") + maxBrightnessColor.B.ToString("X2");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PowerPointLabs/PowerPointLabs/Converters/ColorPane/SelectedColorToMinimumBrightnessHex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Data;
+
+namespace PowerPointLabs.Converters.ColorPane
+{
+    [ValueConversion(typeof(HSLColor), typeof(string))]
+    class SelectedColorToMinimumBrightnessHex : IValueConverter
+    {
+
+        public static SelectedColorToMinimumBrightnessHex Instance = new SelectedColorToMinimumBrightnessHex();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                Color color = (HSLColor)value;
+                return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+            }
+            HSLColor selectedColor = (HSLColor)value;
+            HSLColor minBrightnessHslColor = new HSLColor();
+            minBrightnessHslColor.Hue = selectedColor.Hue;
+            minBrightnessHslColor.Saturation = selectedColor.Saturation;
+            minBrightnessHslColor.Luminosity = 0;
+            Color minBrightnessColor = minBrightnessHslColor;
+            return "#" + minBrightnessColor.R.ToString("X2") + minBrightnessColor.G.ToString("X2") + minBrightnessColor.B.ToString("X2");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
