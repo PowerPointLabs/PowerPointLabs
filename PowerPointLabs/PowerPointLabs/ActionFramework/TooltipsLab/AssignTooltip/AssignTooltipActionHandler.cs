822155f04166529ee816f87713609164fa2053fc
Chester Sng <chester_nutty@hotmail.com>
2019-02-11 13:01:09 +0800

    Change from CreateTooltip to AssignTooltip

--- PowerPointLabs/PowerPointLabs/ActionFramework/TooltipsLab/AssignTooltip/AssignTooltipActionHandler.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/TooltipsLab/AssignTooltip/AssignTooltipActionHandler.cs
@@ -1,8 +1,11 @@
-﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+﻿using Microsoft.Office.Interop.PowerPoint;
+using PowerPointLabs.ActionFramework.Common.Attribute;
 using PowerPointLabs.ActionFramework.Common.Extension;
 using PowerPointLabs.ActionFramework.Common.Interface;
-using PowerPointLabs.AnimationLab;
+using PowerPointLabs.Models;
 using PowerPointLabs.TextCollection;
+using PowerPointLabs.TooltipsLab;
+using PowerPointLabs.Utils;
 
 namespace PowerPointLabs.ActionFramework.TooltipsLab
 {
@@ -11,9 +14,15 @@ namespace PowerPointLabs.ActionFramework.TooltipsLab
     {
         protected override void ExecuteAction(string ribbonId)
         {
-            this.StartNewUndoEntry();
-            
-            AnimateInSlide.AddAnimationInSlide();
+            Selection selection = this.GetCurrentSelection();
+            PowerPointSlide currentSlide = this.GetCurrentSlide();
+
+            if (!ShapeUtil.IsSelectionShape(selection))
+            {
+                return;
+            }
+
+            AttachTriggerAnimation.AddTriggerAnimation(currentSlide, selection);
         }
     }
 }
