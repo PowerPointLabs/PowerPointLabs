082a7042c52e3b53ffef7a9ab5d222a92e0c12c2
Yu Pei Henry <yupeihenry@gmail.com>
2019-02-20 20:42:03 +0800

    Omit use of Globals.

--- PowerPointLabs/PowerPointLabs/TooltipsLab/AddTextbox.cs
+++ PowerPointLabs/PowerPointLabs/TooltipsLab/AddTextbox.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Office.Core;
 using Microsoft.Office.Interop.PowerPoint;
-using PowerPointLabs.ActionFramework.TooltipsLab;
 using PowerPointLabs.Models;
 using PowerPointLabs.Utils;
 
@@ -22,8 +21,10 @@ namespace PowerPointLabs.TooltipsLab
         public static void AddTextboxToCallout(PowerPointSlide currentSlide, Shape callout)
         {
             Shape textbox = AddTextboxToSlide(currentSlide, callout.Left, callout.Top, callout.Width, callout.Height);
-            Selection selection = AddTextboxActionHandler.GetNewSelection(callout, textbox);
-            Shape group = selection.ShapeRange.Group();
+            string[] rangeArray = new string[2];
+            rangeArray[0] = callout.Name;
+            rangeArray[1] = textbox.Name;
+            Shape group = currentSlide.Shapes.Range(rangeArray).Group();
             group.ZOrder(MsoZOrderCmd.msoSendBackward);
             textbox.Select(MsoTriState.msoTrue);
         }
