d826faee8231b0190496fb45144773d1865a5da3
LiXiaowei <lixiaowei0535@gmail.com>
2019-03-17 15:08:55 +0800

    Update e-learning lab icon

--- PowerPointLabs/PowerPointLabs/Properties/Resources.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Resources.Designer.cs
@@ -821,6 +821,16 @@ namespace PowerPointLabs.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap ELearningLabIcon2 {
+            get {
+                object obj = ResourceManager.GetObject("ELearningLabIcon2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         public static byte[] EyeDropper {
