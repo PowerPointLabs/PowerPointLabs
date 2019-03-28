47fb09f001920050d264d4c1111f524eb8e38a12
Yu Pei Henry <yupeihenry@gmail.com>
2019-03-28 23:24:30 +0800

    Fix missing shapeslab thumbnail.

--- PowerPointLabs/PowerPointLabs/ActionFramework/ShapesLab/ShapesLabMenu/ShapesLabMenuImageHandler.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/ShapesLab/ShapesLabMenu/ShapesLabMenuImageHandler.cs
@@ -4,10 +4,10 @@ using PowerPointLabs.ActionFramework.Common.Attribute;
 using PowerPointLabs.ActionFramework.Common.Interface;
 using PowerPointLabs.TextCollection;
 
-namespace PowerPointLabs.ActionFramework.ShapesLab
+namespace PowerPointLabs.ActionFramework.ShapesLab.ShapesLabMenu
 {
-    [ExportImageRibbonId(ShapesLabText.PaneTag)]
-    class ShapesLabImageHandler : ImageHandler
+    [ExportImageRibbonId(ShapesLabText.RibbonMenuId)]
+    class ShapesLabMenuImageHandler : ImageHandler
     {
         protected override Bitmap GetImage(string ribbonId)
         {
