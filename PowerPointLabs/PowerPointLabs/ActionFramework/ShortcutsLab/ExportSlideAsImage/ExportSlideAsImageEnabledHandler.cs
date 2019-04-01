8f1bf0c40efcef9719e138385d6face9f320b689
blewjy <blewjy@gmail.com>
2019-04-01 17:25:31 +0800

    Add icon resource

---
+++ PowerPointLabs/PowerPointLabs/ActionFramework/ShortcutsLab/ExportSlideAsImage/ExportSlideAsImageEnabledHandler.cs
@@ -0,0 +1,17 @@
+﻿using PowerPointLabs.ActionFramework.Common.Attribute;
+using PowerPointLabs.ActionFramework.Common.Extension;
+using PowerPointLabs.ActionFramework.Common.Interface;
+using PowerPointLabs.TextCollection;
+using PowerPointLabs.Utils;
+
+namespace PowerPointLabs.ActionFramework.ShortcutsLab
+{
+    [ExportEnabledRibbonId(ShortcutsLabText.ExportSlideAsImageTag)]
+    class ExportSlideAsImageEnabledHandler : EnabledHandler
+    {
+        protected override bool GetEnabled(string ribbonId)
+        {
+            return true;
+        }
+    }
+}
