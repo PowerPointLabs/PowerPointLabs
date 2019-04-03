561d4383c4edce306c8b9b3494b588966767a839
LiXiaowei <lixiaowei0535@gmail.com>
2019-04-03 13:07:42 +0800

    Remove extra spaces

--- PowerPointLabs/PowerPointLabs/ThisAddIn.cs
+++ PowerPointLabs/PowerPointLabs/ThisAddIn.cs
@@ -920,7 +920,7 @@ namespace PowerPointLabs
             // TODO: variables only at program starts
             NotesToCaptions.IsRemoveCaptionsEnabled = SlidesInRangeHaveCaptions(sldRange);
             ComputerVoiceRuntimeService.IsRemoveAudioEnabled = SlidesInRangeHaveAudio(sldRange);
-            
+
             // update recorder pane
             if (sldRange.Count > 0)
             {
