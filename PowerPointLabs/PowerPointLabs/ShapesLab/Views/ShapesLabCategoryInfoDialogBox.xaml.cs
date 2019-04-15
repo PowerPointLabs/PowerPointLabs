21b1c68082fc724d6ddda4d22b09b2627a1daede
EliasRojasC <eliasrc2002@outlook.com>
2019-04-15 01:30:44 -0500

    #1844 Reserved names Windows files can be used in ShapesLab categories (#1845)
    * Added check to Reserved filenames in shapelab.
    
    Added the check to the category creation in shape lab to not cause errors in file creation.
    
    * Added reserved file names to invalidchars Regex
    
    Added the Windows reserved file names to the invalid chars regular expression and added the reserved file names to the error message.
    
    * Removed odd .htm file
    
    Just deleted it.
    
    * Added case insensitivity
    
    Added a term to the regex so that it does not care about case.
    
    * Fixed the Regex so that all cases are recognized
    
    The regex to prevent windows reserved file names was fixed so that all cases and terms are matched.
    
    * Deleted Comment
    
    Self explanatory.
    
    * Removed unnecessary using

--- PowerPointLabs/PowerPointLabs/ShapesLab/Views/ShapesLabCategoryInfoDialogBox.xaml.cs
+++ PowerPointLabs/PowerPointLabs/ShapesLab/Views/ShapesLabCategoryInfoDialogBox.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 using System.Windows;
-
 using PowerPointLabs.ActionFramework.Common.Extension;
 using PowerPointLabs.TextCollection;
 
@@ -24,9 +23,9 @@ namespace PowerPointLabs.ShapesLab.Views
         // | (vertical bar or pipe)
         // ? (question mark)
         // * (asterisk)
+        // Windows reserved file names.
 
-        // Regex = [<>:"/\\|?*]
-        private const string InvalidCharsRegex = "[<>:\"/\\\\|?*]";
+        private const string InvalidCharsRegex = "^.*(?i)(CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5|LPT6|LPT7|LPT8|LPT9)(?-i).*$|[<>:\"/\\\\|?*]";
         private bool shouldUseExistingCategory;
 
         public ShapesLabCategoryInfoDialogBox()
