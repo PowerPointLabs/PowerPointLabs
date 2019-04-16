5b49ddfe90d5a3028459692a1a73ba02a4297f00
blewjy <blewjy@gmail.com>
2019-04-16 22:25:36 +0800

    dev-release-5.0.0.0

--- PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
+++ PowerPointLabs/PowerPointLabs/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace PowerPointLabs.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("4.10.1.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5.0.0.0")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
