7479dece9173f808dd31f89cc72ce405b353d3f1
Yu Hong Herald <yuhongherald@gmail.com>
2019-05-24 09:37:47 +0800

    add shape copy attributes

--- PowerPointLabs/PowerPointLabs/ActionFramework/Common/Extension/PowerpointExtension.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/Common/Extension/PowerpointExtension.cs
@@ -37,12 +37,51 @@ namespace PowerPointLabs.ActionFramework.Common.Extension
         // Referred to code in ColorsLabPaneWPFxaml
         public static void CopyColorFrom(this Shape newShape, Shape shape)
         {
+            // TODO: Brute force copy fill and line
+            // Some stuff not safe
             newShape.Fill.ForeColor = shape.Fill.ForeColor;
+
             newShape.Line.ForeColor = shape.Line.ForeColor;
+            newShape.Line.DashStyle = shape.Line.DashStyle;
+            newShape.Line.Style = shape.Line.Style;
+            newShape.Line.Transparency = shape.Line.Transparency;
+            newShape.Line.Visible = shape.Line.Visible;
+            newShape.Line.Weight = shape.Line.Weight;
+            newShape.Line.BackColor = shape.Line.BackColor;
+            newShape.Line.Style = shape.Line.Style;
+
+
+            newShape.Line.Pattern = shape.Line.Pattern;
+
             if (shape.TextFrame.HasText == Microsoft.Office.Core.MsoTriState.msoTrue)
             {
-                newShape.TextFrame.TextRange.TrimText().Font.Color.RGB = shape.TextFrame.TextRange.TrimText().Font.Color.RGB;
+                Font newShapeFont = newShape.TextFrame.TextRange.Font;
+                Font shapeFont = shape.TextFrame.TextRange.Font;
+                newShapeFont.CopyFontFrom(shapeFont);
             }
+        }
+
+        // a brute force copy
+        public static void CopyFontFrom(this Font font, Font other)
+        {
+            font.AutoRotateNumbers = other.AutoRotateNumbers;
+            font.BaselineOffset = other.BaselineOffset;
+            font.Bold = other.Bold;
+            font.Emboss = other.Emboss;
+            font.Italic = other.Italic;
+            font.Shadow = other.Shadow;
+            font.Subscript = other.Subscript;
+            font.Superscript = other.Superscript;
+            font.Underline = other.Underline;
+            font.Color.CopyColorFrom(other.Color);
+        }
+
+        public static void CopyColorFrom(this ColorFormat color, ColorFormat other)
+        {
+            color.Brightness = other.Brightness;
+            color.RGB = other.RGB;
+            color.SchemeColor = other.SchemeColor;
+            color.TintAndShade = other.TintAndShade;
         }
 
         public static bool IsEmptyPlaceholder(this Shape shape)
