416677fcace3e1cf5965f6c678b15bc152c0176d
Yu Hong Herald <yuhongherald@gmail.com>
2019-06-04 10:24:01 +0800

    allow hidden content to update colors

--- PowerPointLabs/PowerPointLabs/ColorTheme/ThemeExtensions.cs
+++ PowerPointLabs/PowerPointLabs/ColorTheme/ThemeExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -33,6 +32,7 @@ namespace PowerPointLabs.ColorThemes.Extensions
             if (element.IsUpdated(theme)) { return; }
             switch (element)
             {
+                // textbox placeholder vanishes
                 case TextBlock t:
                     t.Foreground = new SolidColorBrush(theme.foreground);
                     break;
@@ -47,7 +47,7 @@ namespace PowerPointLabs.ColorThemes.Extensions
                 case Control c:
                     c.Background = new SolidColorBrush(theme.background);
                     c.Foreground = new SolidColorBrush(theme.foreground);
-                    c.UpdateColorsControl(sender, theme);
+                    c.UpdateColorsVisual(sender, theme);
                     break;
                 case Panel p:
                     p.Background = new SolidColorBrush(theme.boxBackground);
@@ -97,14 +97,13 @@ namespace PowerPointLabs.ColorThemes.Extensions
         }
 
         // Uses VisualChildren, as some elements are ommitted if logical chlidren are used
-        private static void UpdateColorsControl(this Control element, object sender, ColorTheme theme)
+        private static void UpdateColorsVisual(this Control element, object sender, ColorTheme theme)
         {
-            // For optimization reasons there is no need for the following line of code
-            //element.ResubscribeColorChangedHandlerControl(sender, theme);
             foreach (Visual visual in GetVisualChildCollection<Visual>(element))
             {
                 visual.ApplyTheme(sender, theme);
             }
+            element.ApplyTemplate();
         }
 
         // Uses LogicalChildren as it is much cheaper
@@ -126,14 +125,9 @@ namespace PowerPointLabs.ColorThemes.Extensions
                     listBox.UpdateColorsForChildren(sender, theme);
                 }
             });
-            EventHandler<DataTransferEventArgs> TargetUpdatedHandler = new EventHandler<DataTransferEventArgs>((_o, _e) =>
-            {
-                listBox.UpdateColorsForChildren(sender, theme);
-            });
             ActionCommand command = new ActionCommand(() =>
             {
                 listBox.ItemContainerGenerator.StatusChanged -= StatusChangedHandler;
-                //listBox.TargetUpdated -= TargetUpdatedHandler;
             });
             CommandBinding commandBinding = new CommandBinding() { Command = command };
             foreach (CommandBinding binding in listBox.CommandBindings)
@@ -143,28 +137,7 @@ namespace PowerPointLabs.ColorThemes.Extensions
             listBox.CommandBindings.Clear();
             listBox.UpdateColorsForChildren(sender, theme);
             listBox.ItemContainerGenerator.StatusChanged += StatusChangedHandler;
-            //listBox.TargetUpdated += TargetUpdatedHandler;
             listBox.CommandBindings.Add(commandBinding);
-        }
-
-        private static void ResubscribeColorChangedHandlerControl(this Control control, object sender, ColorTheme theme)
-        {
-            EventHandler<DataTransferEventArgs> TargetUpdatedHandler = new EventHandler<DataTransferEventArgs>((_o, _e) =>
-            {
-                control.UpdateColorsControl(sender, theme);
-            });
-            ActionCommand command = new ActionCommand(() =>
-            {
-                control.TargetUpdated -= TargetUpdatedHandler;
-            });
-            CommandBinding commandBinding = new CommandBinding() { Command = command };
-            foreach (CommandBinding binding in control.CommandBindings)
-            {
-                binding.Command.Execute(null);
-            }
-            control.CommandBindings.Clear();
-            control.TargetUpdated += TargetUpdatedHandler;
-            control.CommandBindings.Add(commandBinding);
         }
 
         private static void UpdateColorsForChildren(this ListBox listBox, object sender, ColorTheme theme)
@@ -178,6 +151,7 @@ namespace PowerPointLabs.ColorThemes.Extensions
                 {
                     element.UpdateColors(sender, theme);
                 }
+                visual.UpdateColors(sender, theme);
             }
         }
 
