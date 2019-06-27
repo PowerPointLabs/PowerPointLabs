8f7fd471063e7b40062ad669811f12fc2027bad2
Yu Hong Herald <yuhongherald@gmail.com>
2019-06-27 15:50:48 +0800

    wip

--- PowerPointLabs/PowerPointLabs/ActionFramework/TooltipsLab/CreateTooltip/CreateTooltipActionHandler.cs
+++ PowerPointLabs/PowerPointLabs/ActionFramework/TooltipsLab/CreateTooltip/CreateTooltipActionHandler.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Linq;
+using Microsoft.Office.Interop.PowerPoint;
 using PowerPointLabs.ActionFramework.Common.Attribute;
 using PowerPointLabs.ActionFramework.Common.Extension;
 using PowerPointLabs.ActionFramework.Common.Interface;
@@ -25,6 +26,12 @@ namespace PowerPointLabs.ActionFramework.TooltipsLab
             }
             
             PowerPoint.Shape triggerShape = CreateTooltip.GenerateTriggerShape(currentSlide);
+            // Look for a shape on the same position of the same size and type on the same slide
+            currentSlide.Shapes
+            foreach (Shape shape in currentSlide.Shapes)
+            {
+                shape.Type
+            }
             PowerPoint.Shape callout = CreateTooltip.GenerateCalloutWithReferenceTriggerShape(currentSlide, triggerShape);
             ConvertToTooltip.AddTriggerAnimation(currentSlide, triggerShape, callout);
 
