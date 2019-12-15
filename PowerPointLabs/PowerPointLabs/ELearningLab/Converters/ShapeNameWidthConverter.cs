1ea3106283d23f1a5fae97022b97cc70e2a6914e
Lim Daekoon <daekoon_lim@hotmail.com>
2019-12-15 21:14:38 +0800

    Create ShapeNameWidthConverter
    This commit creates a class that helps calculate the exact width for
    the textblock that displays the ShapeName for eLearningLab's side
    panel.

---
+++ PowerPointLabs/PowerPointLabs/ELearningLab/Converters/ShapeNameWidthConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PowerPointLabs.ELearningLab.Converters
+{
+    public class ShapeNameWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                return (double)value - 45;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                return (double)value + 45;
+            }
+            return null;
+        }
+    }
+}
