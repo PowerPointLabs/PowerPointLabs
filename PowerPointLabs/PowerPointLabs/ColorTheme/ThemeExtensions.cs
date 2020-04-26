5091dadbc6d3d11de25454b8deb2fc2e2dc65ec4
Muhammad Irham Rasyidi Bin Zainal <h0910104@gmail.com>
2020-04-26 20:59:22 +0800

    Remove old ApplyTheme method

--- PowerPointLabs/PowerPointLabs/ColorTheme/ThemeExtensions.cs
+++ PowerPointLabs/PowerPointLabs/ColorTheme/ThemeExtensions.cs
@@ -45,6 +45,28 @@ namespace PowerPointLabs.ColorThemes.Extensions
         }
 
         /// <summary>
+        /// Applies a theme to the specified FrameworkElement by updating its Resources.
+        /// </summary>
+        /// <param name="element">The WPF FrameworkElement to apply the theme to.</param>
+        /// <param name="sender">The object that triggered the event.</param>
+        /// <param name="theme">The ColorTheme to apply.</param>
+        public static void ApplyTheme(this FrameworkElement element, object sender, ColorTheme theme)
+        {
+            // Three things to update:
+            // 1. The ThemeResourceDictionary at the front of the element's Resources.
+            // 2. MahApps resorce dictionaries if the element has them.
+            // 3. Calling the element's own OnThemeChanged method if it implements INotifyOnThemeChanged.
+
+            element.UpdateThemeResourceDictionary(theme);
+            element.UpdateMahAppsTheme(theme);
+
+            if (element is INotifyOnThemeChanged)
+            {
+                element.Dispatcher.Invoke(new Action(() => (element as INotifyOnThemeChanged).OnThemeChanged(theme)));
+            }
+        }
+
+        /// <summary>
         /// Removes any existing ThemeResourceDictionary in the specified element's Merged Dictionaries and
         /// prepends a ThemeResourceDictionary based on the specified color theme to the Merged Dictionaries
         /// </summary>
@@ -77,8 +99,9 @@ namespace PowerPointLabs.ColorThemes.Extensions
         /// Updates the MahApps Theme in the specified FrameworkElement based on the specified colorTheme.
         /// </summary>
         /// <remarks>
-        /// If the FrameworkElement did not contain any MahApps Resource Dictionaries prior to this method's invocation,
-        /// the FrameworkElement will be left unchanged.
+        /// This method will only set either the BaseDark or the BaseLight theme to the specified element. 
+        /// If the FrameworkElement did not contain any MahApps Resource Dictionaries prior to this method's 
+        /// invocation, the FrameworkElement will be left unchanged.
         /// </remarks>
         /// <param name="element">The element whose Merged Dictionaries will be updated (if applicable).</param>
         /// <param name="colorTheme">The current ColorTheme of the Application</param>
@@ -234,365 +257,9 @@ namespace PowerPointLabs.ColorThemes.Extensions
             element.InvalidateVisual();
         }
 
-        /// <summary>
-        /// Applies a theme to the specified FrameworkElement by updating its Resources.
-        /// </summary>
-        /// <param name="element">The WPF FrameworkElement to apply the theme to.</param>
-        /// <param name="sender">The object that triggered the event.</param>
-        /// <param name="theme">The ColorTheme to apply.</param>
-        public static void ApplyTheme(this FrameworkElement element, object sender, ColorTheme theme)
-        {
-            // Three things to update:
-            // 1. The ThemeResourceDictionary at the front of the element's Resources.
-            // 2. MahApps resorce dictionaries if the element has them.
-            // 3. Calling the element's own OnThemeChanged method if it implements INotifyOnThemeChanged.
-
-            element.UpdateThemeResourceDictionary(theme);
-            element.UpdateMahAppsTheme(theme);
-
-            if (element is INotifyOnThemeChanged)
-            {
-                element.Dispatcher.Invoke(new Action(() => (element as INotifyOnThemeChanged).OnThemeChanged(theme)));
-            }
-        }
-
-        /// <summary>
-        /// Applies a theme to the element recursively.
-        /// </summary>
-        /// <param name="element">WPF element to apply theme to.</param>
-        /// <param name="sender">Object that triggered the event</param>
-        /// <param name="theme">Color theme to update with.</param>
-        public static void ApplyTheme(this DependencyObject element, object sender, ColorTheme theme)
-        {
-            if (!element.Dispatcher.CheckAccess())
-            {
-                element.Dispatcher.Invoke(() => element.ApplyTheme(sender, theme));
-                return;
-            }
-            if (element.IsUpdated(theme)) { return; }
-            RemoveConflictingTheme(element);
-
-            if (element is TextBox)
-            {
-                TextBox t = element as TextBox;
-                t.Foreground = new SolidColorBrush(theme.foreground);
-                t.Background = Brushes.Transparent;
-            }
-            else if (element is TextBlock)
-            {
-                TextBlock t = element as TextBlock;
-                t.Foreground = new SolidColorBrush(theme.foreground);
-                t.Background = Brushes.Transparent;
-            }
-            else if (element is Label)
-            {
-                Label l = element as Label;
-                l.Foreground = new SolidColorBrush(theme.foreground);
-                l.Background = Brushes.Transparent;
-                l.BorderBrush = Brushes.Transparent;
-            }
-            else if (element is ListBox)
-            {
-                ListBox l = element as ListBox;
-                l.Background = new SolidColorBrush(theme.background);
-                l.Foreground = new SolidColorBrush(theme.foreground);
-                l.ResubscribeColorChangedHandler(sender, theme);
-            }
-            else if (element is Frame)
-            {
-                Frame f = element as Frame;
-                f.Background = new SolidColorBrush(theme.background);
-                f.Foreground = new SolidColorBrush(theme.foreground);
-                f.ResubscribeColorChangedHandler(sender, theme);
-            }
-            else if (element is Window)
-            {
-                Window w = element as Window;
-                w.Background = new SolidColorBrush(theme.background);
-                w.Foreground = new SolidColorBrush(theme.foreground);
-                w.UpdateColors(sender, theme);
-            }
-            else if (element is Panel)
-            {
-                Panel p = element as Panel;
-                p.Background = new SolidColorBrush(theme.background);
-                p.UpdateColors(sender, theme); // the window is being update but doesn't show correctly
-            }
-            else if (element is Page)
-            {
-                Page p = element as Page;
-                p.Foreground = new SolidColorBrush(theme.foreground);
-                p.Background = new SolidColorBrush(theme.background);
-                p.UpdateColorsVisual(sender, theme);
-            }
-            else if (element is Control)
-            {
-                Control c = element as Control;
-                c.Background = new SolidColorBrush(theme.background);
-                c.Foreground = new SolidColorBrush(theme.foreground);
-                c.BorderBrush = new SolidColorBrush(theme.foreground);
-                c.UpdateColorsVisual(sender, theme);
-            }
-            else if (element is Border)
-            {
-                (element as Border).Background = new SolidColorBrush(theme.background);
-            }
-            else if (element is Path)
-            {
-                (element as Path).Stroke = new SolidColorBrush(theme.foreground);
-            }
-        }
-
         public static Control GetIWpfControl(this object control)
         {
             return (control as IWpfContainer)?.WpfControl;
-        }
-
-        /// <summary>
-        /// Checks if an element has been updated with the current theme.
-        /// </summary>
-        /// <param name="element">WPF element to be updated</param>
-        /// <param name="theme">Color theme</param>
-        /// <returns></returns>
-        public static bool IsUpdated(this DependencyObject element, ColorTheme theme)
-        {
-            if (element is TextBlock)
-            {
-                return (element as TextBlock).Foreground.IsBrushColor(theme.foreground);
-            }
-            else if (element is Label)
-            {
-                return (element as Label).Foreground.IsBrushColor(theme.foreground);
-            }
-            else if (element is ListBox)
-            {
-                ListBox l = element as ListBox;
-                return l.Background.IsBrushColor(theme.background) &&
-                    l.Foreground.IsBrushColor(theme.foreground);
-            }
-            else if (element is Frame)
-            {
-                Frame f = element as Frame;
-                return f.Background.IsBrushColor(theme.background) &&
-                    f.Foreground.IsBrushColor(theme.foreground);
-            }
-            else if (element is Window)
-            {
-                Window w = element as Window;
-                return w.Background.IsBrushColor(theme.background) &&
-                    w.Foreground.IsBrushColor(theme.foreground);
-            }
-            else if (element is Panel)
-            {
-                return (element as Panel).Background.IsBrushColor(theme.boxBackground);
-            }
-            else if (element is Page)
-            {
-                Page p = element as Page;
-                return p.Foreground.IsBrushColor(theme.foreground) &&
-                    p.Background.IsBrushColor(theme.background);
-            }
-            else if (element is Control)
-            {
-                Control c = element as Control;
-                return c.Background.IsBrushColor(theme.background) &&
-                    c.Foreground.IsBrushColor(theme.foreground) &&
-                    c.BorderBrush.IsBrushColor(theme.foreground);
-            }
-            else if (element is Border)
-            {
-                return (element as Border).Background.IsBrushColor(theme.boxBackground);
-            }
-            else if (element is Path)
-            {
-                return (element as Path).Stroke.IsBrushColor(theme.foreground);
-            }
-            return true;
-        }
-
-        // hotfix for combobox for AudioSettingsDialogWindow
-        private static void RemoveConflictingTheme(DependencyObject element)
-        {
-            if (element is AudioSettingsDialogWindow)
-            {
-                AudioSettingsDialogWindow window = (AudioSettingsDialogWindow)element;
-                Page p = window.MainPage;
-                ResourceDictionary r = p.Resources.MergedDictionaries.FirstOrDefault(
-                    (d) => d.Source.AbsoluteUri == "pack://application:,,,/MahApps.Metro;component/Styles/Controls.xaml");
-                p.Resources.MergedDictionaries.Remove(r);
-            }
-        }
-
-        private static bool IsBrushColor(this Brush b, Color color)
-        {
-            return b is SolidColorBrush && ((SolidColorBrush)b).Color == color;
-        }
-
-        // Uses VisualChildren, as some elements are ommitted if logical chlidren are used
-        private static void UpdateColorsVisual(this FrameworkElement element, object sender, ColorTheme theme)
-        {
-            foreach (Visual visual in GetVisualChildCollection<Visual>(element))
-            {
-                visual.ApplyTheme(sender, theme);
-            }
-            element.ApplyTemplate();
-        }
-
-        // Uses LogicalChildren as it is much cheaper
-        private static void UpdateColors(this DependencyObject element, object sender, ColorTheme theme)
-        {
-            foreach (DependencyObject dependencyObject in GetLogicalChildCollection<DependencyObject>(element))
-            {
-                dependencyObject.ApplyTheme(sender, theme);
-            }
-        }
-
-        private static void ResubscribeColorChangedHandler(this Frame frame, object sender, ColorTheme theme)
-        {
-            EventHandler statusChangedHandler = new EventHandler((_o, _e) =>
-            {
-                if (frame.Content != null)
-                {
-                    frame.UpdateColorsChildren(sender, theme);
-                }
-            });
-            ActionCommand command = new ActionCommand(() =>
-            {
-                frame.ContentRendered -= statusChangedHandler;
-            });
-            CommandBinding commandBinding = new CommandBinding() { Command = command };
-            foreach (CommandBinding binding in frame.CommandBindings)
-            {
-                binding.Command.Execute(null);
-            }
-            frame.CommandBindings.Clear();
-            frame.ContentRendered += statusChangedHandler;
-            frame.CommandBindings.Add(commandBinding);
-        }
-
-        private static void UpdateColorsChildren(this Frame frame, object sender, ColorTheme theme)
-        {
-            Visual visual = frame.Content as Visual;
-            if (visual == null) { return; }
-            foreach (Visual element in GetVisualChildCollection<Visual>(visual))
-            {
-                element.UpdateColors(sender, theme);
-            }
-            visual.UpdateColors(sender, theme);
-        }
-
-        // Exploits the CommandBindings on Control to store actions to unsubscribe events
-        private static void ResubscribeColorChangedHandler(this ListBox listBox, object sender, ColorTheme theme)
-        {
-            EventHandler statusChangedHandler = new EventHandler((_o, _e) =>
-            {
-                if (listBox.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated)
-                {
-                    listBox.UpdateColorsChildren(sender, theme);
-                }
-            });
-            ActionCommand command = new ActionCommand(() =>
-            {
-                listBox.ItemContainerGenerator.StatusChanged -= statusChangedHandler;
-            });
-            CommandBinding commandBinding = new CommandBinding() { Command = command };
-            foreach (CommandBinding binding in listBox.CommandBindings)
-            {
-                binding.Command.Execute(null);
-            }
-            listBox.CommandBindings.Clear();
-            listBox.UpdateColorsChildren(sender, theme);
-            listBox.ItemContainerGenerator.StatusChanged += statusChangedHandler;
-            listBox.CommandBindings.Add(commandBinding);
-        }
-
-        private static void UpdateColorsChildren(this ListBox listBox, object sender, ColorTheme theme)
-        {
-            listBox.UpdateColors(sender, theme);
-            for (int i = 0; i < listBox.Items.Count; i++)
-            {
-                Visual visual = listBox.ItemContainerGenerator.ContainerFromIndex(i) as Visual;
-                if (visual == null) { break; }
-                foreach (Visual element in GetVisualChildCollection<Visual>(visual))
-                {
-                    element.UpdateColors(sender, theme);
-                }
-                visual.UpdateColors(sender, theme);
-            }
-        }
-
-        private static IEnumerable<T> GetChildCollection<T>(DependencyObject parent) where T : DependencyObject
-        {
-            foreach (object child in LogicalTreeHelper.GetChildren(parent))
-            {
-                if (child is DependencyObject)
-                {
-                    DependencyObject depChild = child as DependencyObject;
-                    if (child is T)
-                    {
-                        yield return child as T;
-                    }
-                    foreach (T childOfChild in GetChildCollection<T>(depChild))
-                    {
-                        yield return childOfChild;
-                    }
-                }
-            }
-
-            if (parent is Visual || parent is Visual3D)
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
-                {
-                    DependencyObject depChild = VisualTreeHelper.GetChild(parent, i);
-                    if (depChild is T)
-                    {
-                        yield return depChild as T;
-                    }
-                    foreach (T childOfChild in GetChildCollection<T>(depChild))
-                    {
-                        yield return childOfChild;
-                    }
-                }
-            }
-        }
-
-        private static IEnumerable<T> GetVisualChildCollection<T>(Visual parent) where T : Visual
-        {
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
-            {
-                Visual depChild = VisualTreeHelper.GetChild(parent, i) as Visual;
-                if (depChild == null)
-                {
-                    continue;
-                }
-                if (depChild is T)
-                {
-                    yield return depChild as T;
-                }
-                foreach (T childOfChild in GetVisualChildCollection<T>(depChild))
-                {
-                    yield return childOfChild;
-                }
-            }
-        }
-
-        private static IEnumerable<T> GetLogicalChildCollection<T>(DependencyObject parent) where T : DependencyObject
-        {
-            foreach (object child in LogicalTreeHelper.GetChildren(parent))
-            {
-                if (child is DependencyObject)
-                {
-                    DependencyObject depChild = child as DependencyObject;
-                    if (child is T)
-                    {
-                        yield return child as T;
-                    }
-                    foreach (T childOfChild in GetLogicalChildCollection<T>(depChild))
-                    {
-                        yield return childOfChild;
-                    }
-                }
-            }
         }
     }
 }
