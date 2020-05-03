a87b82c91a7041eceb64d3a87d136a474ed94685
Muhammad Irham Rasyidi Bin Zainal <h0910104@gmail.com>
2020-05-04 06:16:34 +0800

    Rearrange using statements in ThemeResourceDictionary

--- PowerPointLabs/PowerPointLabs/ColorTheme/ThemeResourceDictionary.cs
+++ PowerPointLabs/PowerPointLabs/ColorTheme/ThemeResourceDictionary.cs
@@ -1,10 +1,10 @@
-﻿using PowerPointLabs.ActionFramework.Common.Log;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using PowerPointLabs.ActionFramework.Common.Log;
 
 namespace PowerPointLabs.ColorThemes
 {
